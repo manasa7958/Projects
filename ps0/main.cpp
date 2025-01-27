6d3c18b7f25c1451ce0bd6166a58263530e76dcb
manasa7958 <manasapraveen.work@gmail.com>
2025-01-27 17:20:51 -0500

    Update main.cpp

--- ps0/main.cpp
+++ ps0/main.cpp
@@ -3,21 +3,32 @@
 int main()
 {
 	// Demo Code
-	sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
+	sf::RenderWindow demoWindow(sf::VideoMode({200, 200}), "SFML works!");
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Green);
 
 	//Extended Code - Idea is a spaceship going into space, New feature add some audio for takeoff and background changes in light blue to navy/black
 	sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML works!");
+	
+	//Loading spaceship sprite
 	const sf::Texture texture("spaceship.png");
 	sf::Sprite sprite(texture);
+
+	// Load a music to play
+    	sf::Music music("launch.wav");
 	
-	while (window.isOpen())
+	// Play the music
+	music.play();
+	
+	while (demoWindow.isOpen() && window.isOpen())
 	{
-		while (const std::optional event = window.pollEvent())
+		while (const std::optional event = demoWindow.pollEvent() || const std::optional event = window.pollEvent())
 		{
 			if (event->is<sf::Event::Closed>())
+			{
 				window.close();
+				demoWindow.close();
+			}
 		}
 		// Responding to keystrokes
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
