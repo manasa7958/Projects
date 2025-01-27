fe8cb7285528556e36a6e519c46f321cdf153dd7
manasa7958 <manasapraveen.work@gmail.com>
2025-01-27 16:58:27 -0500

    Update main.cpp
    new feature unlocked 👍 :))

--- ps0/main.cpp
+++ ps0/main.cpp
@@ -2,37 +2,42 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+	// Demo Code
+	sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-                window.close();
-        }
-	// Responding to keystrokes
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+	//Extended Code - Idea is a spaceship going into space, New feature add some audio for takeoff and background changes in light blue to navy/black
+	sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML works!");
+	const sf::Texture texture("spaceship.png");
+	sf::Sprite sprite(texture);
+	
+	while (window.isOpen())
 	{
-		shape.move(0.0f, -2.0f);
+		while (const std::optional event = window.pollEvent())
+		{
+			if (event->is<sf::Event::Closed>())
+				window.close();
+		}
+		// Responding to keystrokes
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+		{
+			shape.move(0.0f, -2.0f);
+		}
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+		{
+			shape.move(0.0f, 2.0f);
+		}
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+		{
+			shape.move(-2.0f, 0.0f);
+		}
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+		{
+			shape.move(2.0f, 0.0f);
+		}
+		window.clear(); 
+		window.draw(shape);
+		window.display();
 	}
-	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-	{
-		shape.move(0.0f, 2.0f);
-	}
-	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-	{
-		shape.move(-2.0f, 0.0f);
-	}
-	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-	{
-		shape.move(2.0f, 0.0f);
-	}
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
 }
