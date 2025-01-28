63737c74fec865615d415d9c6ac7789ef2ad8689
Manasa Praveen <manasapraveen09@gmail.com>
2025-01-27 23:21:01 -0500

    linting errors

--- ps0/main.cpp
+++ ps0/main.cpp
@@ -1,94 +1,81 @@
 // Copyright 2025 Manasa Praveen
 
-#include <iostream>
-#include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
 #include <SFML/System/Clock.hpp>
+#include <iostream>
 
+int main() {
+  // Demo Code
+  sf::RenderWindow demoWindow(sf::VideoMode({200, 200}), "SFML works!");
+  sf::CircleShape shape(100.f);
+  shape.setFillColor(sf::Color::Green);
 
-int main()
-{
-	// Demo Code
-	sf::RenderWindow demoWindow(sf::VideoMode({200, 200}), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+  // Extended Code - Idea is a spaceship going into space, New feature add some
+  // audio for takeoff
+  sf::RenderWindow window(sf::VideoMode({800, 600}),
+                          "Rocket launch is a success!");
 
-	//Extended Code - Idea is a spaceship going into space, New feature add some audio for takeoff
-	sf::RenderWindow window(sf::VideoMode({800, 600}), "Rocket launch is a success!");
-	
-	//Loading spaceship sprite
-	sf::Texture texture;
-	// Error if image doesn't load
-	if (!texture.loadFromFile("spaceship.png"))
-	{
-		std::cerr << "Error! Image could not be loaded" << std::endl;
-		return -1;
-	}
-	sf::Sprite sprite(texture);
-	sprite.setScale(sf::Vector2f(0.3f, 0.3f));
-	float speed = 1.f; //speed for sprite
+  // Loading spaceship sprite
+  sf::Texture texture;
+  // Error if image doesn't load
+  if (!texture.loadFromFile("spaceship.png")) {
+    std::cerr << "Error! Image could not be loaded" << std::endl;
+    return -1;
+  }
+  sf::Sprite sprite(texture);
+  sprite.setScale(sf::Vector2f(0.3f, 0.3f));
+  float speed = 1.f; // speed for sprite
 
-	// Load launch music
-    	sf::Music music;
-	// Error if image doesn't load
-	if (!music.openFromFile("launch.wav"))
-	{
-		std::cerr << "Error! Audio could not be loaded" << std::endl;
-		return -1;
-	}
-	music.play();
+  // Load launch music
+  sf::Music music;
+  // Error if image doesn't load
+  if (!music.openFromFile("launch.wav")) {
+    std::cerr << "Error! Audio could not be loaded" << std::endl;
+    return -1;
+  }
+  music.play();
 
-	sf::Clock clock;
-	sf::Color color = sf::Color(165, 229, 255);
-	
-	while (demoWindow.isOpen() || window.isOpen())
-	{
-		while (const std::optional<sf::Event> event = demoWindow.pollEvent())
-		{
-			if (event->is<sf::Event::Closed>())
-			{
-				demoWindow.close();
-			}
-		}
-		while (const std::optional<sf::Event> event = window.pollEvent())
-		{
-			if (event->is<sf::Event::Closed>())
-			{
-				window.close();
-			}
-		}
+  sf::Clock clock;
+  sf::Color color = sf::Color(165, 229, 255);
 
-		if (clock.getElapsedTime().asSeconds() > 4.0f)
-		{
-			color = sf::Color(0, 71, 100);
-			window.clear(color); // changing background color to Storm
-			clock.stop();
-		}
-		
-		// Responding to keystrokes
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))  
-		{
-			sprite.move(sf::Vector2f(-speed, 0));
-		}
-		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up))
-		{
-			sprite.move(sf::Vector2f(0, -speed)); // weird but the idea is we start at the top left corner and +y means you move down
-		}
-		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down))
-		{
-			sprite.move(sf::Vector2f(0, speed));
-		}
-		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
-		{
-			sprite.move(sf::Vector2f(speed, 0));
-		}
-		demoWindow.clear(); 
-		demoWindow.draw(shape);
-		demoWindow.display();
-		
-		window.clear(color); // changing background color to Lambent
-		window.draw(sprite);
-		window.display();
-	}
-	return 0;
+  while (demoWindow.isOpen() || window.isOpen()) {
+    while (const std::optional<sf::Event> event = demoWindow.pollEvent()) {
+      if (event->is<sf::Event::Closed>()) {
+        demoWindow.close();
+      }
+    }
+    while (const std::optional<sf::Event> event = window.pollEvent()) {
+      if (event->is<sf::Event::Closed>()) {
+        window.close();
+      }
+    }
+
+    if (clock.getElapsedTime().asSeconds() > 4.0f) {
+      color = sf::Color(0, 71, 100);
+      window.clear(color); // changing background color to Storm
+      clock.stop();
+    }
+
+    // Responding to keystrokes
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left)) {
+      sprite.move(sf::Vector2f(-speed, 0));
+    } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up)) {
+      sprite.move(
+          sf::Vector2f(0, -speed)); // weird but the idea is we start at the top
+                                    // left corner and +y means you move down
+    } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down)) {
+      sprite.move(sf::Vector2f(0, speed));
+    } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right)) {
+      sprite.move(sf::Vector2f(speed, 0));
+    }
+    demoWindow.clear();
+    demoWindow.draw(shape);
+    demoWindow.display();
+
+    window.clear(color); // changing background color to Lambent
+    window.draw(sprite);
+    window.display();
+  }
+  return 0;
 }
