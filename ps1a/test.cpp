4b6df3c0ffd549528df1fb957c25ff1a75b9f238
manasa7958 <manasapraveen.work@gmail.com>
2025-01-29 12:10:57 -0500

    Update test.cpp

--- ps1a/test.cpp
+++ ps1a/test.cpp
@@ -1,1 +1,33 @@
+// Copyright 2022
+// By Dr. Rykalova
+// Editted by Dr. Daly
+// test.cpp for PS1a
+// updated 1/8/2024
 
+#include <iostream>
+#include <string>
+
+#include "FibLFSR.hpp"
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE Main
+#include <boost/test/unit_test.hpp>
+
+using PhotoMagic::FibLFSR;
+
+BOOST_AUTO_TEST_CASE(testStepInstr) {
+  FibLFSR l("1011011000110110");
+  BOOST_REQUIRE_EQUAL(l.step(), 0);
+  BOOST_REQUIRE_EQUAL(l.step(), 0);
+  BOOST_REQUIRE_EQUAL(l.step(), 0);
+  BOOST_REQUIRE_EQUAL(l.step(), 1);
+  BOOST_REQUIRE_EQUAL(l.step(), 1);
+  BOOST_REQUIRE_EQUAL(l.step(), 0);
+  BOOST_REQUIRE_EQUAL(l.step(), 0);
+  BOOST_REQUIRE_EQUAL(l.step(), 1);
+}
+
+BOOST_AUTO_TEST_CASE(testGenerateInstr) {
+  FibLFSR l("1011011000110110");
+  BOOST_REQUIRE_EQUAL(l.generate(9), 51);
+}
