6aced9fcac3985fbe41a194ee3adf368c191bbff
manasa7958 <manasapraveen.work@gmail.com>
2025-01-30 16:59:03 -0500

    Update FibLFSR.cpp

--- ps1a/FibLFSR.cpp
+++ ps1a/FibLFSR.cpp
@@ -1,1 +1,11 @@
+#include <iostream>
+#include <string>
+
+#include "FibLFSR.hpp"
+
+namespace PhotoMagic {
+    int FibLFSR::TAP1 = 13;
+    int FibLFSR::TAP2 = 12;
+    int FibLFSR::TAP3 = 10;
+}
 
