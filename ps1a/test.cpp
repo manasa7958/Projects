d5a1c2a12bd1d1ced8e92a09ad3227462ce317a8
manasa7958 <manasapraveen.work@gmail.com>
2025-02-03 00:01:42 -0500

    Update test.cpp

--- ps1a/test.cpp
+++ ps1a/test.cpp
@@ -32,13 +32,13 @@ BOOST_AUTO_TEST_CASE(testLFSROutput) {
     FibLFSR l("1011011000110110");
     std::stringstream ss;
     ss << l;
-    BOOST_REQUIRE_EQUAL(ss.str(), "1011011000110110");
+    BOOST_CHECK_EQUAL(ss.str(), "1011011000110110");
 }
 
 BOOST_AUTO_TEST_CASE(testInvalidInputs) {
-  BOOST_REQUIRE_THROW(FibLFSR("101101"), std::invalid_argument);//too short
-  BOOST_REQUIRE_THROW(FibLFSR("10110110001101100"), std::invalid_argument);//too long
-  BOOST_REQUIRE_THROW(FibLFSR("A01101102 110$10"), std::invalid_argument);//other characters
+  BOOST_CHECK_THROW(FibLFSR("101101"), std::invalid_argument);//too short
+  BOOST_CHECK_THROW(FibLFSR("10110110001101100"), std::invalid_argument);//too long
+  BOOST_CHECK_THROW(FibLFSR("A01101102 110$10"), std::invalid_argument);//other characters
 }
 
 BOOST_AUTO_TEST_CASE(testEdgeCases) {
@@ -51,19 +51,19 @@ BOOST_AUTO_TEST_CASE(testStep) {
     FibLFSR l("1011011000110110");
     std::vector<int> expect = {0, 0, 0, 1, 1, 0, 0, 1, 1, 1};
     for (int bit : expect) {
-        BOOST_REQUIRE_EQUAL(l.step(), bit);
+        BOOST_CHECK_EQUAL(l.step(), bit);
     }
 }
 BOOST_AUTO_TEST_CASE(testGenerateConsecutive) {
     FibLFSR l("1011011000110110");
     int first = l.generate(4);
     int second = l.generate(4);
-    BOOST_REQUIRE_NE(first, second);
+    BOOST_CHECK_NE(first, second);
 }
 
 BOOST_AUTO_TEST_CASE(testNoThrow) {
     FibLFSR l("1011011000110110");
-    BOOST_REQUIRE_NO_THROW(l.generate(1));
-    BOOST_REQUIRE_NO_THROW(l.generate(16));
+    BOOST_CHECK_NO_THROW(l.generate(1));
+    BOOST_CHECK_NO_THROW(l.generate(16));
 }
 
