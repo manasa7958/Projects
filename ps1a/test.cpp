a36bf8f1cde0eb4bc942ec809df6cbe0d479ea34
manasa7958 <manasapraveen.work@gmail.com>
2025-02-02 22:49:00 -0500

    Update test.cpp

--- ps1a/test.cpp
+++ ps1a/test.cpp
@@ -29,19 +29,20 @@ BOOST_AUTO_TEST_CASE(testStepInstr) {
 
 BOOST_AUTO_TEST_CASE(testGenerateInstr) {
   FibLFSR l("1011011000110110");
+  BOOST_REQUIRE_EQUAL(l.generate(9), 51);
   BOOST_REQUIRE_THROW(l.generate(-1), std::invalid_argument);
 }
 
 BOOST_AUTO_TEST_CASE(testInvalidLength) {
-  BOOST_REQUIRE_THROW(FibLFSR("101101"), std::invalid_argument);
-  BOOST_REQUIRE_THROW(FibLFSR("10110110001101100"), std::invalid_argument);
+  BOOST_REQUIRE_THROW(FibLFSR("101101"), std::invalid_argument);//too short
+  BOOST_REQUIRE_THROW(FibLFSR("10110110001101100"), std::invalid_argument);//too long
 }
 
 BOOST_AUTO_TEST_CASE(testInvalidCharacters) {
-  BOOST_REQUIRE_THROW(FibLFSR("A011011000110110"), std::invalid_argument);
-  BOOST_REQUIRE_THROW(FibLFSR("A!@#$"), std::invalid_argument);
-  BOOST_REQUIRE_THROW(FibLFSR("1011021100001101"), std::invalid_argument);
-  BOOST_REQUIRE_THROW(FibLFSR("11 00110011001100"), std::invalid_argument);
+  BOOST_REQUIRE_THROW(FibLFSR("A011011000110110"), std::invalid_argument);//other characters
+  BOOST_REQUIRE_THROW(FibLFSR("A!@#$"), std::invalid_argument);//too short and invalid characters
+  BOOST_REQUIRE_THROW(FibLFSR("1011021100001101"), std::invalid_argument);//other characters
+  BOOST_REQUIRE_THROW(FibLFSR("11 00110011001100"), std::invalid_argument);//other characters
 }
 
 BOOST_AUTO_TEST_CASE(testGenerateEdgeCases) {
