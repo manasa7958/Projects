197a203dc8f8f50955a6c00d070be420719ea5d3
manasa7958 <manasapraveen.work@gmail.com>
2025-02-02 23:23:02 -0500

    Update test.cpp

--- ps1a/test.cpp
+++ ps1a/test.cpp
@@ -1,12 +1,7 @@
-// Copyright 2022
-// By Dr. Rykalova
-// Editted by Dr. Daly
-// test.cpp for PS1a
-// updated 1/8/2024
+// Copyright 2025 Manasa Praveen
 
 #include <iostream>
 #include <string>
-
 #include "FibLFSR.hpp"
 
 #define BOOST_TEST_DYN_LINK
@@ -29,22 +24,24 @@ BOOST_AUTO_TEST_CASE(testStepInstr) {
 
 BOOST_AUTO_TEST_CASE(testGenerateInstr) {
   FibLFSR l("1011011000110110");
-  BOOST_REQUIRE_EQUAL(l.generate(9), 51);
   BOOST_REQUIRE_THROW(l.generate(-1), std::invalid_argument);
 }
 
-BOOST_AUTO_TEST_CASE(testInvalidLength) {
-  BOOST_REQUIRE_THROW(FibLFSR("101101"), std::invalid_argument);//too short
-  BOOST_REQUIRE_THROW(FibLFSR("10110110001101100"), std::invalid_argument);//too long
+BOOST_AUTO_TEST_CASE(testLFSROutput) {
+    FibLFSR l("1011011000110110");
+    stringstream ss;
+    ss << l;
+    BOOST_REQUIRE_EQUAL(ss.str(), "1011011000110110");
 }
 
-BOOST_AUTO_TEST_CASE(testInvalidCharacters) {
-  BOOST_REQUIRE_THROW(FibLFSR("A011011000110110"), std::invalid_argument);//other characters
-  BOOST_REQUIRE_THROW(FibLFSR("A!@#$"), std::invalid_argument);//too short and invalid characters
-  BOOST_REQUIRE_THROW(FibLFSR("1011021100001101"), std::invalid_argument);//other characters
-  BOOST_REQUIRE_THROW(FibLFSR("11 00110011001100"), std::invalid_argument);//other characters
+BOOST_AUTO_TEST_CASE(testInvalidInputs) {
+  BOOST_REQUIRE_THROW(FibLFSR 1("101101"), std::invalid_argument);//too short
+  BOOST_REQUIRE_THROW(FibLFSR 1("10110110001101100"), std::invalid_argument);//too long
+  BOOST_REQUIRE_THROW(FibLFSR 1("A011011000110110"), std::invalid_argument);//other characters
+  BOOST_REQUIRE_THROW(FibLFSR 1("A!@#$"), std::invalid_argument);//too short and invalid characters
+  BOOST_REQUIRE_THROW(FibLFSR 1("1011021100001101"), std::invalid_argument);//other characters
+  BOOST_REQUIRE_THROW(FibLFSR 1("11 00110011001100"), std::invalid_argument);//other characters
 }
-
 BOOST_AUTO_TEST_CASE(testGenerateEdgeCases) {
     FibLFSR l("1100110011001100");
     BOOST_REQUIRE_EQUAL(l.generate(1), l.step());
