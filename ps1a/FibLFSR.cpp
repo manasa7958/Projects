ce606cbacc0f2e344385789c26efe6a6cd684a8b
manasa7958 <manasapraveen.work@gmail.com>
2025-02-02 22:20:31 -0500

    Update FibLFSR.cpp

--- ps1a/FibLFSR.cpp
+++ ps1a/FibLFSR.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <string>
-
+#include <stdexcept>
 #include "FibLFSR.hpp"
 
 namespace PhotoMagic {
 
-FibLFSR::::FibLFSR(const std::string& seed) {
+FibLFSR::FibLFSR(const std::string& seed) {
     if (seed.length() != 16 || seed.find_first_not_of("01") != std::string::npos) {
         throw std::invalid_argument("Seed should be 16-bits");
     }
@@ -17,8 +17,10 @@ int FibLFSR::step() {
     int bit1 = registerBits[TAP1] - '0';
     int bit2 = registerBits[TAP2] - '0';
     int bit3 = registerBits[TAP3] - '0';
+    
     int newBit = bit0 ^ bit1 ^ bit2 ^ bit3;
     registerBits = registerBits.substr(1) + std::to_string(newBit);
+    
     return newBit;
 }
 
@@ -26,6 +28,7 @@ int FibLFSR::generate(int k) {
     if (k <= 0) {
         throw std::invalid_argument("K should be greater than 0");
     }
+    
     int result = 0;
     for (int i = 0; i < k; i++) {
         result = (result << 1) | step();
