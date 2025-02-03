e3e2b91946f368100e631274cf6646051739a1d3
manasa7958 <manasapraveen.work@gmail.com>
2025-02-02 23:25:24 -0500

    Update FibLFSR.cpp

--- ps1a/FibLFSR.cpp
+++ ps1a/FibLFSR.cpp
@@ -6,24 +6,24 @@
 namespace PhotoMagic {
 
 FibLFSR::FibLFSR(const std::string& seed) {
-    if (seed.length() != 16 || seed.find_first_not_of("01") != std::string::npos) {
+    /*if (seed.length() != 16 || seed.find_first_not_of("01") != std::string::npos) {
         throw std::invalid_argument("Seed should be 16-bits");
     }
-    registerBits = seed;
+    registerBits = seed;*/
 }
 
 int FibLFSR::step() {
-    int leftmostBit = registerBits[0] - '0';
-    int tapBit1 = registerBits[TAP1] - '0';
-    int tapBit2 = registerBits[TAP2] - '0';
-    int tapBit3 = registerBits[TAP3] - '0';
-    int newBit = leftmostBit ^ tapBit1 ^ tapBit2 ^ tapBit3;
-    registerBits = registerBits.substr(1) + std::to_string(newBit);
-    return newBit;
+    /*int bit0 = registerBits[0] - '0';
+    int bit1 = registerBits[TAP1] - '0';
+    int bi2 = registerBits[TAP2] - '0';
+    int bit3 = registerBits[TAP3] - '0';
+    int newBit = bit0 ^ bit1 ^ bit2 ^ bit3;
+    registerBits = registerBits.substr(1) + std::to_string(newBit);*/
+    return 0;
 }
 
 int FibLFSR::generate(int k) {
-    if (k <= 0) {
+    /*if (k <= 0) {
         throw std::invalid_argument("k should be greater than 0");
     }
 
@@ -32,12 +32,12 @@ int FibLFSR::generate(int k) {
         int bit = step();
         std::cout << bit;
         result = (result << 1) | bit;
-    }
-    return result;
+    }*/
+    return 0;
 }
 
 std::ostream& operator<<(std::ostream& os, const FibLFSR& lfsr) {
-    os << lfsr.registerBits;
+    /*os << lfsr.registerBits;*/
     return os;
 }
 }
