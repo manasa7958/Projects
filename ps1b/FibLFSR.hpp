fc983e18e60d6ae80c05246d8fb752b8ca283e44
manasa7958 <manasapraveen.work@gmail.com>
2025-02-04 14:38:29 -0500

    Create FibLFSR.hpp

---
+++ ps1b/FibLFSR.hpp
@@ -0,0 +1,19 @@
+// Copyright 2025 Manasa Praveen
+#pragma once
+
+#include <iostream>
+#include <string>
+
+namespace PhotoMagic {
+class FibLFSR {
+ public:
+  explicit FibLFSR(const std::string& seed);
+  explicit FibLFSR(unsigned int seed);  // Optional
+  static FibLFSR fromPassword(const std::string& password);  // Optional
+  int step();
+  int generate(int steps);
+  friend std::ostream& operator<<(std::ostream&, const FibLFSR& lfsr);
+ private:
+  std::string registerBits;
+};
+}  // namespace PhotoMagic
