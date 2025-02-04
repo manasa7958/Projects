209256e8e3791c64bba466a03bf810731350e555
manasa7958 <manasapraveen.work@gmail.com>
2025-02-03 23:07:04 -0500

    Update FibLFSR.hpp

--- ps1a/FibLFSR.hpp
+++ ps1a/FibLFSR.hpp
@@ -10,10 +10,9 @@ class FibLFSR {
   explicit FibLFSR(const std::string& seed);
   explicit FibLFSR(unsigned int seed);  // Optional
   static FibLFSR fromPassword(const std::string& password);  // Optional
-  friend std::ostream& operator<<(std::ostream&, const FibLFSR& lfsr);
   int step();
   int generate(int steps);
-
+  friend std::ostream& operator<<(std::ostream&, const FibLFSR& lfsr);
  private:
   std::string registerBits;
 };
