7e00ee895d137eebf6d21da66ce969adf982e59b
manasa7958 <manasapraveen.work@gmail.com>
2025-02-03 19:32:45 -0500

    Update main.cpp

--- ps1a/main.cpp
+++ ps1a/main.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+#include "FibLFSR.hpp"
 
+int main() {
+    PhotoMagic::FibLFSR lfsr("");
+    std::cout <<Initl state: " " << lfsr <<std::endl;
+    lfsr.step();
+    std::cout <<Initl state: " " << lfsr <<std::endl;
+    int value = lfsr.generate(5);
+    std::cout <<Initl state: " " << lfsr <<std::endl;
+    return 0;
+}
