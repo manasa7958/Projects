695cf0498d7c44a5bb3672a0b3a0050324139cee
Manasa Praveen <manasapraveen09@gmail.com>
2025-02-03 20:33:56 -0500

    linting

--- ps1a/FibLFSR.hpp
+++ ps1a/FibLFSR.hpp
@@ -7,20 +7,20 @@
 namespace PhotoMagic {
 class FibLFSR {
  public:
-    explicit FibLFSR(const std::string& seed);
-    explicit FibLFSR(unsigned int seed); // Optional
+  explicit FibLFSR(const std::string& seed);
+  explicit FibLFSR(unsigned int seed);  // Optional
 
-    static FibLFSR fromPassword(const std::string& password); // Optional
+  static FibLFSR fromPassword(const std::string& password);  // Optional
 
-    int step();
-    int generate(int k);
-    std::ostream& print(std::ostream& os) const;
+  int step();
+  int generate(int k);
+  std::ostream& print(std::ostream& os) const;
 
  private:
-   std::string registerBits;
+  std::string registerBits;
 };
 
-inline std::ostream& operator<<(std::ostream& os, const FibLFSR& lfsr){
-   return lfsr.print(os);
+inline std::ostream& operator<<(std::ostream& os, const FibLFSR& lfsr) {
+  return lfsr.print(os);
 }
 }  // namespace PhotoMagic
