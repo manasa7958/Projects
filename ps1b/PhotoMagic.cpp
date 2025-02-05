eacf21db65e38f4055c0d8800a77203cce4b497c
manasa7958 <manasapraveen.work@gmail.com>
2025-02-05 15:22:57 -0500

    Update PhotoMagic.cpp

--- ps1b/PhotoMagic.cpp
+++ ps1b/PhotoMagic.cpp
@@ -3,18 +3,18 @@
 
 namespace PhotoMagic {
 
-void transform(sf::Image& img, FibLFSR* lfsr) {
-    sf::Vector2u size = img.getSize();
+void transform(sf::Image& image, FibLFSR* lfsr) {
+    sf::Vector2u size = image.getSize();
     
     for (unsigned int x = 0; x < size.x; ++x) {
         for (unsigned int y = 0; y < size.y; ++y) {
-            sf::Color pixel = img.getPixel(x, y);
+            sf::Color pixel = image.getPixel(x, y);
             
             pixel.r ^= lfsr->generate(8);
             pixel.g ^= lfsr->generate(8);
             pixel.b ^= lfsr->generate(8);
             
-            img.setPixel(x, y, pixel);
+            image.setPixel(x, y, pixel);
         }
     }
 }
