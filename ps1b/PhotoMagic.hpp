18716816a55d25816cb5cc53a5d8682927e2e2de
manasa7958 <manasapraveen.work@gmail.com>
2025-02-10 19:46:46 -0500

    Update PhotoMagic.hpp

--- ps1b/PhotoMagic.hpp
+++ ps1b/PhotoMagic.hpp
@@ -1,9 +1,10 @@
 // Copyright 2025 Manasa Praveen
 #pragma once
+#include <SFML/Graphics.hpp>
 #include <iostream>
 #include <algorithm>
 #include "FibLFSR.hpp"
-#include <SFML/Graphics.hpp>
+
 
 
 namespace PhotoMagic {
