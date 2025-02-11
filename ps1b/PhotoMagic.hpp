3867f8d625d14a08e0ae1b2552964170b13c1755
manasa7958 <manasapraveen.work@gmail.com>
2025-02-10 19:49:52 -0500

    Update PhotoMagic.hpp

--- ps1b/PhotoMagic.hpp
+++ ps1b/PhotoMagic.hpp
@@ -1,11 +1,11 @@
 // Copyright 2025 Manasa Praveen
 #pragma once
-#include <SFML/Graphics.hpp>
-#include <iostream>
-#include <algorithm>
 #include "FibLFSR.hpp"
 
+#include <iostream>
+#include <algorithm>
 
+#include <SFML/Graphics.hpp>
 
 namespace PhotoMagic {
 void transform(sf::Image& img, FibLFSR* lfsr);
