7ab1a5bf4aa97fb803a0d2432d6e719f32520aea
manasa7958 <manasapraveen.work@gmail.com>
2025-02-11 19:37:01 -0500

    Update triangle.cpp

--- ps2/triangle.cpp
+++ ps2/triangle.cpp
@@ -2,36 +2,40 @@
 #include "triangle.hpp"
 #include <cmath>
 
+//Constructor intialization
 Triangle::Triangle(double length, int depth) : length(length), depth(depth) {
     generate();
 }
 
+//generate function defintion for the main triangle
 void Triangle::generate() {
-    sf::Vector2f p1(400, 100);
-    sf::Vector2f p2(400 - length / 2, 100 + length * sqrt(3) / 2);
-    sf::Vector2f p3(400 + length / 2, 100 + length * sqrt(3) / 2);
-    fractal(p1, p2, p3, depth);
+    sf::Vector2f v1(400, 100); //top
+    sf::Vector2f v2(400 - length / 2, 100 + length * sqrt(3) / 2); //bottom left
+    sf::Vector2f v3(400 + length / 2, 100 + length * sqrt(3) / 2); //bottom right
+    fractal(v1, v2, v3, depth);
 }
 
-void Triangle::fractal(sf::Vector2f p1, sf::Vector2f p2, sf::Vector2f p3, int level) {
+//recursive function
+void Triangle::fractal(sf::Vector2f v1, sf::Vector2f v2, sf::Vector2f v3, int level) {
     if (level == 0) {
         sf::VertexArray triangle(sf::Triangles, 3);
-        triangle[0].position = p1;
-        triangle[1].position = p2;
-        triangle[2].position = p3;
+        triangle[0].position = v1;
+        triangle[1].position = v2;
+        triangle[2].position = v3;
         triangles.push_back(triangle);
         return;
     }
     
-    sf::Vector2f mid1((p1 + p2) / 2.0f);
-    sf::Vector2f mid2((p2 + p3) / 2.0f);
-    sf::Vector2f mid3((p3 + p1) / 2.0f);
+    sf::Vector2f mid1((v1 + v2) / 2.0f);
+    sf::Vector2f mid2((v2 + v3) / 2.0f);
+    sf::Vector2f mid3((v3 + v1) / 2.0f);
     
-    fractal(p1, mid1, mid3, level - 1);
-    fractal(mid1, p2, mid2, level - 1);
-    fractal(mid3, mid2, p3, level - 1);
+    fractal(v1, mid1, mid3, level - 1);
+    fractal(mid1, v2, mid2, level - 1);
+    fractal(mid3, mid2, v3, level - 1);
 }
 
+//draw the triangles
 void Triangle::draw(sf::RenderTarget& target, sf::RenderStates states) const {
     for (const auto& triangle : triangles) {
         target.draw(triangle, states);
