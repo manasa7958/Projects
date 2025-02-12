955529d5e1dbc74234830d46357e3b9b5328dfff
manasa7958 <manasapraveen.work@gmail.com>
2025-02-11 19:25:58 -0500

    Update main.cpp

--- ps2/main.cpp
+++ ps2/main.cpp
@@ -1,1 +1,32 @@
+// main.cpp
+#include <SFML/Graphics.hpp>
+#include "triangle.hpp"
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " <L> <N>\n";
+        return 1;
+    }
+    
+    double length = std::stod(argv[1]);
+    int depth = std::stoi(argv[2]);
+    
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Triangle Fractal");
+    Triangle fractalTriangle(length, depth);
+    
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        
+        window.clear();
+        window.draw(fractalTriangle);
+        window.display();
+    }
+    
+    return 0;
+}
 
