185999a88a93d25ad68860d8c0bcbe38d3ff92de
manasa7958 <manasapraveen.work@gmail.com>
2025-02-17 12:56:04 -0500

    Update triangle.hpp

--- ps2/triangle.hpp
+++ ps2/triangle.hpp
@@ -7,12 +7,12 @@
 
 class Triangle : public sf::Drawable {
 public:
-    Triangle(double length, int iterate);
+    Triangle(double length, int depth);
     void generate(); //to draw big triangle
 
 private:
     double length;
-    int iterate;
+    int depth;
     std::vector<sf::VertexArray> triangles; //this holds the three vertices of each triangle
     void fractal(sf::Vector2f v1, sf::Vector2f v2, sf::Vector2f v3, int level); // for smaller triangles (recursive funciton)
     virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
