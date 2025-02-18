ad44482806d3cbe85a8ba4cd50f309c7ae01009e
manasa7958 <manasapraveen.work@gmail.com>
2025-02-18 14:46:11 -0500

    Update triangle.cpp

--- ps2/triangle.cpp
+++ ps2/triangle.cpp
@@ -3,18 +3,18 @@
 #include "triangle.hpp"
 
 Triangle::Triangle(float x, float y, float size) : m_x(x), m_y(y), m_size(size) {
-    // Create main triangle
+    // Create main triangle
     m_triangles.push_back(createTriangle(x, y, size));
-    generateFractal(x, y, size, 3); // Increased depth to 3 to generate 1/8th sized triangles
+    generateFractal(x, y, size, 3); // Increased depth to 3 to generate 1/8th sized triangles
 }
 
 void Triangle::setPosition(float x, float y) {
-    m_x = x;
-    m_y = y;
-    // Clear and recreate triangles at new position
-    m_triangles.clear();
-    m_triangles.push_back(createTriangle(x, y, m_size));
-    generateFractal(x, y, m_size, 3);
+    m_x = x;
+    m_y = y;
+    // Clear and recreate triangles at new position
+    m_triangles.clear();
+    m_triangles.push_back(createTriangle(x, y, m_size));
+    generateFractal(x, y, m_size, 3);
 }
 
 void Triangle::generateFractal(float x, float y, float size, int depth) {
