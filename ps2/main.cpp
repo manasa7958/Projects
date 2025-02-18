d9dc81a4357aa0c08172f4f82b759d241820c350
manasa7958 <manasapraveen.work@gmail.com>
2025-02-18 14:09:33 -0500

    Update main.cpp

--- ps2/main.cpp
+++ ps2/main.cpp
@@ -64,7 +64,7 @@ int main(int argc, char* argv[]) {
 #include <SFML/Graphics.hpp>
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Level 2 Triangle Fractal");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Level 2 Triangle Fractal");
     Triangle fractal(400.0f, 400.0f, 200.0f);
     fractal.generateLevel2();
     while (window.isOpen()) {
