47a286a60807481f83c7c98f198a9247af070ac7
manasa7958 <manasapraveen.work@gmail.com>
2025-02-18 14:01:34 -0500

    Update triangle.hpp

--- ps2/triangle.hpp
+++ ps2/triangle.hpp
@@ -22,7 +22,7 @@ private:
 
 #endif*/ // TRIANGLE_HPP
 
-// triangle.hpp
+/*// triangle.hpp
 #ifndef TRIANGLE_HPP
 #define TRIANGLE_HPP
 #include <vector>
@@ -36,6 +36,39 @@ public:
 private:
     std::vector<sf::ConvexShape> triangles;
     virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+};
+
+#endif // TRIANGLE_HPP*/
+
+//triangle.hpp
+#ifndef TRIANGLE_HPP
+#define TRIANGLE_HPP
+
+#include <SFML/Graphics.hpp>
+#include <vector>
+
+class Triangle : public sf::Drawable {
+public:
+    // Constructor with position and size
+    Triangle(float x, float y, float size);
+    
+    // Set the position of the triangle
+    void setPosition(float x, float y);
+    
+    // Generate level 2 triangles
+    void generateLevel2();
+
+private:
+    // Helper to create a single triangle
+    sf::ConvexShape createTriangle(float x, float y, float size) const;
+    
+    // Override draw method from sf::Drawable
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+    
+    // Member variables
+    std::vector<sf::ConvexShape> m_triangles;
+    float m_x, m_y;
+    float m_size;
 };
 
 #endif // TRIANGLE_HPP
