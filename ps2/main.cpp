014b3080f71c7fb9b9c05bc7ba02ba318ac21cf7
manasa7958 <manasapraveen.work@gmail.com>
2025-02-18 14:40:13 -0500

    Update main.cpp

--- ps2/main.cpp
+++ ps2/main.cpp
@@ -59,23 +59,29 @@ int main(int argc, char* argv[]) {
     return 0;
 }*/
 
-//main.cpp
+//main.c
 #include "triangle.hpp"
 #include <SFML/Graphics.hpp>
 
 int main() {
+    // Create window
     sf::RenderWindow window(sf::VideoMode(800, 600), "Level 2 Triangle Fractal");
+    
+    // Create triangle at center of window
     Triangle fractal(400.0f, 400.0f, 200.0f);
-    fractal.generateLevel2();
+    
+    // Main loop
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+        
         window.clear(sf::Color::White);
         window.draw(fractal);
         window.display();
     }
+    
     return 0;
 }
