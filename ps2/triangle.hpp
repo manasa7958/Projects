fb7c639dc7e172a20a2b350be788ba573a2656b7
manasa7958 <manasapraveen.work@gmail.com>
2025-02-18 14:42:15 -0500

    Update triangle.hpp

--- ps2/triangle.hpp
+++ ps2/triangle.hpp
@@ -18,28 +18,10 @@ private:
     void fractal(std::array<sf::Vector2f, 3> vertices, int level);
     virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
 };
-
-#endif*/ // TRIANGLE_HPP
-
-/*// triangle.hpp
-#ifndef TRIANGLE_HPP
-#define TRIANGLE_HPP
-#include <vector>
-#include <SFML/Graphics.hpp>
-
-class Triangle : public sf::Drawable {
-public:
-    Triangle(float x, float y, float size, int depth);
-    void generateFractal(float x, float y, float size, int depth);
-
-private:
-    std::vector<sf::ConvexShape> triangles;
-    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
-};
-
-#endif // TRIANGLE_HPP*/
+*/
 
 //triangle.hpp
+
 #ifndef TRIANGLE_HPP
 #define TRIANGLE_HPP
 
@@ -48,16 +30,25 @@ private:
 
 class Triangle : public sf::Drawable {
 public:
-    Triangle(float x, float y, float size, int depth);
+    // Constructor with position and size
+    Triangle(float x, float y, float size);
+    // Set the position of the triangle
     void setPosition(float x, float y);
-    void generateLevel2();
 
 private:
+    // Helper to create a single triangle
+    sf::ConvexShape createTriangle(float x, float y, float size) const;
+    
+    // Recursive function to generate fractal pattern
+    void generateFractal(float x, float y, float size, int depth);
+    
+    // Override draw method from sf::Drawable
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+    
+    // Member variables
+    std::vector<sf::ConvexShape> m_triangles;
     float m_x, m_y;
     float m_size;
-    int depth;
-    std::vector<sf::ConvexShape> m_triangles;
-    sf::ConvexShape createTriangle(float x, float y, float size) const;
-    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
 };
+
 #endif // TRIANGLE_HPP
