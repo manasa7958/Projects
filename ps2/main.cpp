01d33e76f20ddb9c43802d41614c7e5379b632d7
manasa7958 <manasapraveen.work@gmail.com>
2025-02-18 15:56:56 -0500

    Update main.cpp

--- ps2/main.cpp
+++ ps2/main.cpp
@@ -4,34 +4,6 @@
 #include "triangle.hpp"
 
 int main() {
-    // Create window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Level 2 Triangle Fractal");
-    
-    // Create triangle at center of window
-    Triangle fractal(400.0f, 400.0f, 200.0f);
-    
-    // Main loop
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-        window.clear(sf::Color::White);
-        window.draw(fractal);
-        window.display();
-    }
-    
-    return 0;
-}
-
-
-// Copyright 2025 Manasa Praveen
-#include <iostream>
-#include <SFML/Graphics.hpp>
-#include "triangle.hpp"
-
-int main() {
     int depth;
     std::cout << "Enter the fractal depth (recommended 1-7 for performance): ";
     std::cin >> depth;
