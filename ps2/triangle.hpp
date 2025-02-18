debd0a367bdfd09b2915e6ceeca797143163041b
manasa7958 <manasapraveen.work@gmail.com>
2025-02-18 14:57:08 -0500

    Update triangle.hpp

--- ps2/triangle.hpp
+++ ps2/triangle.hpp
@@ -1,32 +1,9 @@
 // Copyright 2025 Manasa Praveen
-/*#ifndef TRIANGLE_HPP
-#define TRIANGLE_HPP
-
-#include <vector>
-#include <SFML/Graphics.hpp>
-
-class Triangle : public sf::Drawable {
-public:
-    Triangle(double length, int depth);
-    void generate();
-
-private:
-    double length;
-    int depth;
-    float angle;
-    std::vector<sf::VertexArray> triangles;
-    void fractal(std::array<sf::Vector2f, 3> vertices, int level);
-    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
-};
-*/
-
-//triangle.hpp
-
 #ifndef TRIANGLE_HPP
 #define TRIANGLE_HPP
 
-#include <SFML/Graphics.hpp>
 #include <vector>
+#include <SFML/Graphics.hpp>
 
 class Triangle : public sf::Drawable {
 public:
