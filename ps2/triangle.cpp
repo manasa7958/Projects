417559c92f409a93176910d52fa512c102d44b63
manasa7958 <manasapraveen.work@gmail.com>
2025-02-17 19:49:06 -0500

    Update triangle.cpp

--- ps2/triangle.cpp
+++ ps2/triangle.cpp
@@ -87,15 +87,14 @@ void Triangle::generateFractal(float x, float y, float size, int depth) {
         triangles.push_back(triangle);
     } else {
         float newSize = size / 2;
-        float height = newSize * sqrt(3) / 2;  // Corrected height calculation!
+        float height = newSize * sqrt(3) / 2;  // Correct height calculation
 
-        // Place child triangles at the correct three vertices
-        generateFractal(x, y, newSize, depth - 1);                      // Bottom-left vertex
-        generateFractal(x + newSize, y, newSize, depth - 1);            // Bottom-right vertex
-        generateFractal(x + newSize / 2, y - height, newSize, depth - 1); // Top vertex
+        // Corrected positions to ensure triangles form at the vertices
+        generateFractal(x - newSize / 2, y + height / 2, newSize, depth - 1); // Bottom-left vertex
+        generateFractal(x + newSize / 2, y + height / 2, newSize, depth - 1); // Bottom-right vertex
+        generateFractal(x, y - height, newSize, depth - 1);                   // Top vertex
     }
 }
-
 
 void Triangle::draw(sf::RenderTarget& target, sf::RenderStates states) const {
     for (const auto& triangle : triangles) {
