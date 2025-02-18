cdfce9078ee9738f0c06959435cdfbf88523f15f
manasa7958 <manasapraveen.work@gmail.com>
2025-02-18 18:57:02 -0500

    Update triangle.hpp

--- ps2/triangle.hpp
+++ ps2/triangle.hpp
@@ -6,25 +6,16 @@
 
 class Triangle : public sf::Drawable {
  public:
-  // Constructor with position and size
   Triangle(float x, float y, float size, int depth);
-  // Set the position of the triangle
   void setPosition(float x, float y);
   void setDepth(int depth);
 
  private:
-  // Helper to create a single triangle
   sf::ConvexShape createTriangle(float x, float y, float size,
                                  sf::Color color) const;
-
-  // Recursive function to generate fractal pattern
   void generateFractal(float x, float y, float size, int depth);
-
-  // Override draw method from sf::Drawable
   void draw(sf::RenderTarget& target,
                     sf::RenderStates states) const override;
-
-  // Member variables
   std::vector<sf::ConvexShape> m_triangles;
   float m_x, m_y;
   float m_size;
