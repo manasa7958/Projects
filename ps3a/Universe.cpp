6329b8b8cae8d45a804e0e283e413c0052522631
manasa7958 <manasapraveen.work@gmail.com>
2025-02-22 17:19:24 -0500

    Update Universe.cpp

--- ps3a/Universe.cpp
+++ ps3a/Universe.cpp
@@ -1,12 +1,31 @@
 #include "Universe.hpp"
-#include <memory>
-#include <vector>
-#include <iostream>
 
 namespace NB {
 
 Universe::Universe() : universeRadius(0) {}
 
+std::istream& operator>>(std::istream& in, Universe& universe) {
+    return in;  // Do nothing, just allow compilation
+}
+
+std::ostream& operator<<(std::ostream& out, const Universe& universe) {
+    return out;  // Do nothing
+}
+
+size_t Universe::size() const { return 0; }
+double Universe::radius() const { return 0; }
+const CelestialBody& Universe::operator[](size_t index) const {
+    throw std::out_of_range("Index out of range"); 
+}
+
+}  // namespace NB
+/*
+namespace NB {
+
+// Default constructor
+Universe::Universe() : universeRadius(0) {}
+
+// Overloaded input operator >>
 std::istream& operator>>(std::istream& in, Universe& universe) {
     int n;
     in >> n >> universe.universeRadius;
@@ -20,6 +39,7 @@ std::istream& operator>>(std::istream& in, Universe& universe) {
     return in;
 }
 
+// Overloaded output operator <<
 std::ostream& operator<<(std::ostream& out, const Universe& universe) {
     out << universe.bodies.size() << " " << universe.universeRadius << std::endl;
     for (const auto& body : universe.bodies) {
@@ -28,11 +48,15 @@ std::ostream& operator<<(std::ostream& out, const Universe& universe) {
     return out;
 }
 
+// Get number of celestial bodies
 size_t Universe::size() const { return bodies.size(); }
+
+// Get radius of the universe
 double Universe::radius() const { return universeRadius; }
 
+// Access a specific celestial body
 const CelestialBody& Universe::operator[](size_t index) const {
     return *bodies.at(index);
 }
 
-}  // namespace NB
+}  // namespace NB*/
