ed029085c625a3981ecb1640736d9f191e661077
manasa7958 <manasapraveen.work@gmail.com>
2025-02-23 17:14:20 -0500

    Update test.cpp

--- ps3a/test.cpp
+++ ps3a/test.cpp
@@ -6,8 +6,6 @@
 #include "Universe.hpp"
 #include "CelestialBody.hpp"
 
-//const double EPSILON = 1e-6;
-
 void testEmpty() {
     std::stringstream input("0 1.0e+11\n");
     NB::Universe universe;
@@ -21,7 +19,6 @@ void testFormatting() {
     std::stringstream input("1.4960e+11 0.0000e+00 0.0000e+00 2.9800e+04 5.9740e+24 earth.gif");
     NB::CelestialBody body;
     input >> body;
-
     std::stringstream output;
     output << std::scientific << std::setprecision(4);
     output << body;
@@ -36,30 +33,6 @@ void testFormatting() {
 
     std::cout << "Formatting test passed!" << std::endl;
 }
-
-/*void testFlipped() {
-    std::stringstream input("1.4960e+11 2.0000e+11 3.0000e+04 4.0000e+04 5.9740e+24 flipped.gif");
-    NB::CelestialBody body;
-    input >> body;
-
-    // Ensure values are assigned correctly
-    assert(std::fabs(body.position().x - 1.4960e+11) < EPSILON);
-    assert(std::fabs(body.position().y - 2.0000e+11) < EPSILON);
-    assert(std::fabs(body.velocity().x - 3.0000e+04) < EPSILON);
-    assert(std::fabs(body.velocity().y - 4.0000e+04) < EPSILON);
-    assert(std::fabs(body.mass() - 5.9740e+24) < EPSILON);
-    assert(body.imageFile == "flipped.gif");
-
-    std::stringstream output;
-    output << body;
-
-    std::cout << "Expected Output: \"1.4960e+11 2.0000e+11 3.0000e+04 4.0000e+04 5.9740e+24 flipped.gif\\n\"" << std::endl;
-    std::cout << "Actual Output:   \"" << output.str() << "\"" << std::endl;
-
-    assert(output.str() == "1.4960e+11 2.0000e+11 3.0000e+04 4.0000e+04 5.9740e+24 flipped.gif\n");
-
-    std::cout << "Test passed!" << std::endl;
-}*/
 
 int main() {
     testEmpty();
