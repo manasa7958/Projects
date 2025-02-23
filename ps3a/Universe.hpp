0de2e6443023d78e520407cdd0f3ed63951c65d8
manasa7958 <manasapraveen.work@gmail.com>
2025-02-23 14:44:41 -0500

    Update Universe.hpp

--- ps3a/Universe.hpp
+++ ps3a/Universe.hpp
@@ -1,11 +1,8 @@
 #pragma once
-
 #include <iostream>
 #include <vector>
 #include <memory>
-
 #include <SFML/Graphics.hpp>
-
 #include "CelestialBody.hpp"
 
 namespace NB {
@@ -13,12 +10,9 @@ class Universe: public sf::Drawable {
  public:
     Universe(); // Required
     explicit Universe(const std::string& filename);  // Optional
-
     size_t size() const; // Optional
     double radius() const; // Optional
-
     const CelestialBody& operator[](size_t i) const; // Optional
-
     void step(double dt); // Implemented in part b, behavior for part a is undefined
  protected:
     void draw(sf::RenderTarget& window, sf::RenderStates states) const override; // From sf::Drawable
