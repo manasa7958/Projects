f78193807a897926c9f28440fb3fcc1f98eb554f
manasa7958 <manasapraveen.work@gmail.com>
2025-02-23 18:42:07 -0500

    Update test.cpp

--- ps3a/test.cpp
+++ ps3a/test.cpp
@@ -20,33 +20,21 @@ BOOST_AUTO_TEST_CASE(testEmptyUniverse) {
     BOOST_TEST(universe.size() == 0);
 }
 
-/*BOOST_AUTO_TEST_CASE(testFormatting) {
-    std::stringstream input("1.4960e+11 0.0000e+00 0.0000e+00 2.9800e+04 5.9740e+24 earth.gif");
-    NB::CelestialBody body;
-    input >> body;
-
-    std::stringstream output;
-    output << std::scientific << std::setprecision(4);
-    output << body;
-
-    std::string expected = "1.4960e+11 0.0000e+00 0.0000e+00 2.9800e+04 5.9740e+24 earth.gif\n";
-    std::string actual = output.str();
-
-    BOOST_TEST(actual == expected);
-}*/
-
 BOOST_AUTO_TEST_CASE(testFlippedValues) {
     std::stringstream input("1.1111e+11 2.2222e+11 3.3333e+04 4.4444e+04 5.5555e+24 flipped.gif");
     NB::CelestialBody body;
     input >> body;
-
-    BOOST_TEST(std::fabs(body.position().x - 1.1111e+11) < EPSILON);
-    BOOST_TEST(std::fabs(body.position().y - 2.2222e+11) < EPSILON);
-    BOOST_TEST(std::fabs(body.velocity().x - 3.3333e+04) < EPSILON);
-    BOOST_TEST(std::fabs(body.velocity().y - 4.4444e+04) < EPSILON);
-    BOOST_TEST(std::fabs(body.mass() - 5.5555e+24) < EPSILON);
-    BOOST_TEST(body.imageFile == "flipped.gif");
+    
+    NB::CelestialBody expected;
+    expected.pos = sf::Vector2f(1.1111e+11, 2.2222e+11);
+    expected.vel = sf::Vector2f(3.3333e+04, 4.4444e+04);
+    expected.m = 5.5555e+24;
+    expected.imageFile = "flipped.gif";
+    
+    BOOST_REQUIRE_CLOSE(body.position().x, expected.position().x, 0.001f);
+    BOOST_REQUIRE_CLOSE(body.position().y, expected.position().y, 0.001f);
+    BOOST_REQUIRE_CLOSE(body.velocity().x, expected.velocity().x, 0.001f);
+    BOOST_REQUIRE_CLOSE(body.velocity().y, expected.velocity().y, 0.001f);
+    BOOST_REQUIRE_CLOSE(body.mass(), expected.mass(), 0.001f);
+    BOOST_REQUIRE(body.imageFile == expected.imageFile);
 }
-
-
-
