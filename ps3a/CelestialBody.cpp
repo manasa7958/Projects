12715ddcb071b781d1b8817424a090c2640699e5
manasa7958 <manasapraveen.work@gmail.com>
2025-02-23 18:22:58 -0500

    Update CelestialBody.cpp

--- ps3a/CelestialBody.cpp
+++ ps3a/CelestialBody.cpp
@@ -3,7 +3,6 @@
 
 namespace NB {
 
-// Constructor with default values
 CelestialBody::CelestialBody() : pos(0, 0), vel(0, 0), m(0), imageFile("") {}
 
 // Overloaded input operator >>
@@ -14,6 +13,7 @@ std::istream& operator>>(std::istream& in, CelestialBody& body) {
     return in;
 }
 
+// Overloaded output operator >>
 std::ostream& operator<<(std::ostream& out, const CelestialBody& body) {
     out << std::scientific << std::setprecision(4) << std::uppercase
         << body.pos.x << " " << body.pos.y << " "
@@ -28,7 +28,6 @@ sf::Vector2f CelestialBody::position() const { return pos; }
 sf::Vector2f CelestialBody::velocity() const { return vel; }
 float CelestialBody::mass() const { return m; }
 
-// Empty draw method (not needed for tests)
 void CelestialBody::draw(sf::RenderTarget& window, sf::RenderStates states) const {}
 
 }  // namespace NB
