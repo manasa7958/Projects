34c674ae94f09064599218a0ddbcaf36fa6bcb01
manasa7958 <manasapraveen.work@gmail.com>
2025-02-23 17:05:28 -0500

    Update test.cpp

--- ps3a/test.cpp
+++ ps3a/test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cassert>
 #include <sstream>
+#include <iomanip>
 #include <cmath> 
 #include "Universe.hpp"
 #include "CelestialBody.hpp"
@@ -14,6 +15,26 @@ void testEmpty() {
 
     assert(universe.size() == 0);
     std::cout << "Empty Universe test passed!" << std::endl;
+}
+
+void testFormatting() {
+    std::stringstream input("1.4960e+11 0.0000e+00 0.0000e+00 2.9800e+04 5.9740e+24 earth.gif");
+    NB::CelestialBody body;
+    input >> body;
+
+    std::stringstream output;
+    output << std::scientific << std::setprecision(4);
+    output << body;
+
+    std::string expected = "1.4960e+11 0.0000e+00 0.0000e+00 2.9800e+04 5.9740e+24 earth.gif\n";
+    std::string actual = output.str();
+
+    std::cout << "Expected: \"" << expected << "\"\n";
+    std::cout << "Actual:   \"" << actual << "\"\n";
+
+    assert(actual == expected);
+
+    std::cout << "Formatting test passed!" << std::endl;
 }
 
 void testFlipped() {
