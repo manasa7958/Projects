f3622ddc5b75a8983c698ef50696d3debbc03bad
manasa7958 <manasapraveen.work@gmail.com>
2025-02-23 14:40:31 -0500

    Update CelestialBody.hpp

--- ps3a/CelestialBody.hpp
+++ ps3a/CelestialBody.hpp
@@ -1,38 +1,5 @@
 #pragma once
-
 #include <iostream>
-#include <SFML/Graphics.hpp>
-
-namespace NB {
-
-class CelestialBody : public sf::Drawable {
-public:
-    explicit CelestialBody(); // Required
-
-    sf::Vector2f getPosition() const; // ✅ Getter for position
-    sf::Vector2f getVelocity() const; // ✅ Getter for velocity
-    float getMass() const; // ✅ Getter for mass
-
-protected:
-    void draw(sf::RenderTarget& window, sf::RenderStates states) const override; // SFML Drawing
-
-private:
-    sf::Vector2f position;  // ✅ Position of the celestial body
-    sf::Vector2f velocity;  // ✅ Velocity of the celestial body
-    float mass;  // ✅ Mass of the celestial body
-    std::string imageFile;  // ✅ Store the planet's image filename
-
-    // ✅ Friend functions to allow direct access to private members
-    friend std::istream& operator>>(std::istream& is, CelestialBody& uni);
-    friend std::ostream& operator<<(std::ostream& os, const CelestialBody& uni);
-};
-
-}  // namespace NB
-
-#pragma once
-
-#include <iostream>
-
 #include <SFML/Graphics.hpp>
 
 namespace NB {
@@ -47,7 +14,14 @@ class CelestialBody: public sf::Drawable {
  protected:
     void draw(sf::RenderTarget& window, sf::RenderStates states) const override; // From sf::Drawable
  private:
-    // Fields and helper methods go here
+    sf::Vector2f pos;  // Position of the celestial body
+    sf::Vector2f vel;  // Velocity of the celestial body
+    float m;  // Mass of the celestial body
+    std::string imageFile;  // Store the planet's image filename
+
+    // Friend functions to allow direct access to private members
+    friend std::istream& operator>>(std::istream& is, CelestialBody& uni);
+    friend std::ostream& operator<<(std::ostream& os, const CelestialBody& uni);
 };
 
 std::istream& operator>>(std::istream& is, CelestialBody& uni);
