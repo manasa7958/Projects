8618273833dae15e7478627d9eb2f9f4210a08b5
manasa7958 <manasapraveen.work@gmail.com>
2025-02-23 14:42:38 -0500

    Update CelestialBody.cpp

--- ps3a/CelestialBody.cpp
+++ ps3a/CelestialBody.cpp
@@ -3,28 +3,28 @@
 namespace NB {
 
 // Constructor with default values
-CelestialBody::CelestialBody() : position(0, 0), velocity(0, 0), mass(0), imageFile("") {}
+CelestialBody::CelestialBody() : pos(0, 0), vel(0, 0), m(0), imageFile("") {}
 
 // Overloaded input operator >>
 std::istream& operator>>(std::istream& in, CelestialBody& body) {
-    in >> body.position.x >> body.position.y
-       >> body.velocity.x >> body.velocity.y
-       >> body.mass >> body.imageFile;
+    in >> body.pos.x >> body.pos.y
+       >> body.vel.x >> body.vel.y
+       >> body.m >> body.imageFile;
     return in;
 }
 
 // Overloaded output operator <<
 std::ostream& operator<<(std::ostream& out, const CelestialBody& body) {
-    out << body.position.x << " " << body.position.y << " "
-        << body.velocity.x << " " << body.velocity.y << " "
-        << body.mass << " " << body.imageFile << std::endl;
+    out << body.pos.x << " " << body.pos.y << " "
+        << body.vel.x << " " << body.vel.y << " "
+        << body.m << " " << body.imageFile << std::endl;
     return out;
 }
 
 // Getter methods
-sf::Vector2f CelestialBody::getPosition() const { return position; }
-sf::Vector2f CelestialBody::getVelocity() const { return velocity; }
-float CelestialBody::getMass() const { return mass; }
+sf::Vector2f CelestialBody::position() const { return pos; }
+sf::Vector2f CelestialBody::velocity() const { return vel; }
+float CelestialBody::mass() const { return m; }
 
 // Empty draw method (not needed for tests)
 void CelestialBody::draw(sf::RenderTarget& window, sf::RenderStates states) const {}
