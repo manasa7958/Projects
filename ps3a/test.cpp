892e842bcb087e0475f57dc6c4edff7725eccc32
manasa7958 <manasapraveen.work@gmail.com>
2025-02-23 16:16:51 -0500

    Update test.cpp

--- ps3a/test.cpp
+++ ps3a/test.cpp
@@ -13,9 +13,19 @@ void testEmpty() {
     assert(universe.size() == 0);
     std::cout << "Empty Universe test passed!" << std::endl;
 }
-
+void testFormatting() {
+    std::stringstream input("1.4960e+11 0.0000e+00 0.0000e+00 2.9800e+04 5.9740e+24 earth.gif");
+    NB::CelestialBody body;
+    input >> body;
+    std::stringstream output;
+    output << body;
+    
+    assert(output.str() == "1.4960e+11 0 0 29800 5.974e+24 earth.gif\n"); // Check if output matches correct format
+    std::cout << "Formatting for CelestialBody passed!" << std::endl;
+}
 int main() {
-    testEmpty();  
+    testEmpty();
+    testFormatting();
     std::cout << "Tests passed!" << std::endl;
     return 0;
 }
