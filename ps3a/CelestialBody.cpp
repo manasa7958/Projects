d7f6bab9dafe977e9022a144d54f33f6c652a7a7
manasa7958 <manasapraveen.work@gmail.com>
2025-02-24 01:30:29 -0500

    Update CelestialBody.cpp

--- ps3a/CelestialBody.cpp
+++ ps3a/CelestialBody.cpp
@@ -3,19 +3,23 @@
 #include "CelestialBody.hpp"
 
 namespace NB {
-CelestialBody::CelestialBody() : pos(0, 0), vel(0, 0), m(0), imageFile("") {}
-std::istream& operator>>(std::istream& in, CelestialBody& body) {
-  in >> body.pos.x >> body.pos.y >> body.vel.x >> body.vel.y >> body.m >>
-      body.imageFile;
-  return in;
-}
-std::ostream& operator<<(std::ostream& out, const CelestialBody& body) {
-  out << std::scientific << std::setprecision(4) << std::uppercase << body.pos.x
-      << " " << body.pos.y << " " << body.vel.x << " " << body.vel.y << " "
-      << body.m << " " << body.imageFile << std::endl;
 
-  return out;
+CelestialBody::CelestialBody() : pos(0, 0), vel(0, 0), m(0), imageFile("") {}
+
+std::istream& operator>>(std::istream& in, CelestialBody& body) {
+in >> body.pos.x >> body.pos.y >> body.vel.x >> body.vel.y >> body.m >>
+    body.imageFile;
+return in;
 }
+
+std::ostream& operator<<(std::ostream& out, const CelestialBody& body) {
+out << std::scientific << std::setprecision(4) << std::uppercase << body.pos.x
+    << " " << body.pos.y << " " << body.vel.x << " " << body.vel.y << " "
+    << body.m << " " << body.imageFile << std::endl;
+return out;
+}
+
 void CelestialBody::draw(sf::RenderTarget& window,
                          sf::RenderStates states) const {}
+
 }  // namespace NB
