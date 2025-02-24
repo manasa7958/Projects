c9a37c6e726a2da9e78e6b19063afb2d65af4b75
manasa7958 <manasapraveen.work@gmail.com>
2025-02-24 01:20:45 -0500

    Update CelestialBody.cpp

--- ps3a/CelestialBody.cpp
+++ ps3a/CelestialBody.cpp
@@ -1,17 +1,14 @@
 // Copyright 2025 Manasa Praveen
-#include "CelestialBody.hpp"
-
 #include <iomanip>
+#include "CelestialBody.hpp"
 
 namespace NB {
 CelestialBody::CelestialBody() : pos(0, 0), vel(0, 0), m(0), imageFile("") {}
-
 std::istream& operator>>(std::istream& in, CelestialBody& body) {
   in >> body.pos.x >> body.pos.y >> body.vel.x >> body.vel.y >> body.m >>
       body.imageFile;
   return in;
 }
-
 std::ostream& operator<<(std::ostream& out, const CelestialBody& body) {
   out << std::scientific << std::setprecision(4) << std::uppercase << body.pos.x
       << " " << body.pos.y << " " << body.vel.x << " " << body.vel.y << " "
@@ -19,8 +16,6 @@ std::ostream& operator<<(std::ostream& out, const CelestialBody& body) {
 
   return out;
 }
-
 void CelestialBody::draw(sf::RenderTarget& window,
                          sf::RenderStates states) const {}
-
 }  // namespace NB
