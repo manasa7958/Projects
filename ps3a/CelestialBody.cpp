349b97178beffb0b14f6723fe6f4792d7be2f2b4
Manasa Praveen <manasapraveen09@gmail.com>
2025-02-25 15:30:40 -0500

    linting

--- ps3a/CelestialBody.cpp
+++ ps3a/CelestialBody.cpp
@@ -1,41 +1,43 @@
 // Copyright 2025 Manasa Praveen
+#include "CelestialBody.hpp"
+
+#include <iomanip>
 #include <iostream>
 #include <memory>
-#include <iomanip>
-#include "CelestialBody.hpp"
 
 namespace NB {
 
 CelestialBody::CelestialBody() : pos(0, 0), vel(0, 0), m(0), imageFile("") {}
 
 std::istream& operator>>(std::istream& in, CelestialBody& body) {
-in >> body.pos.x >> body.pos.y >> body.vel.x >> body.vel.y >> body.m >>
-    body.imageFile;
-return in;
+  in >> body.pos.x >> body.pos.y >> body.vel.x >> body.vel.y >> body.m >>
+      body.imageFile;
+  return in;
 }
 std::ostream& operator<<(std::ostream& out, const CelestialBody& body) {
-out << std::scientific << std::setprecision(4) << std::uppercase << body.pos.x
-    << " " << body.pos.y << " " << body.vel.x << " " << body.vel.y << " "
-    << body.m << " " << body.imageFile << std::endl;
-return out;
+  out << std::scientific << std::setprecision(4) << std::uppercase << body.pos.x
+      << " " << body.pos.y << " " << body.vel.x << " " << body.vel.y << " "
+      << body.m << " " << body.imageFile << std::endl;
+  return out;
 }
 
 bool CelestialBody::loadTexture(double universeRadius) {
-    texture = std::make_shared<sf::Texture>();
-    if (!texture->loadFromFile(imageFile)) {
-        std::cerr << "Failed to load texture " << imageFile << std::endl;
-        return false;
-    }
-    sprite.setTexture(*texture);
-    const double SCALE_FACTOR = 400.0 / universeRadius;
-    float screenX = (pos.x * SCALE_FACTOR) + 400;  // Center in window
-    float screenY = (pos.y * SCALE_FACTOR) + 400;
-    sprite.setPosition(screenX, screenY);
-    return true;
+  texture = std::make_shared<sf::Texture>();
+  if (!texture->loadFromFile(imageFile)) {
+    std::cerr << "Failed to load texture " << imageFile << std::endl;
+    return false;
+  }
+  sprite.setTexture(*texture);
+  const double SCALE_FACTOR = 400.0 / universeRadius;
+  float screenX = (pos.x * SCALE_FACTOR) + 400;  // Center in window
+  float screenY = (pos.y * SCALE_FACTOR) + 400;
+  sprite.setPosition(screenX, screenY);
+  return true;
 }
 
-void CelestialBody::draw(sf::RenderTarget& window, sf::RenderStates states) const {
-    window.draw(sprite, states);
+void CelestialBody::draw(sf::RenderTarget& window,
+                         sf::RenderStates states) const {
+  window.draw(sprite, states);
 }
 
 }  // namespace NB
