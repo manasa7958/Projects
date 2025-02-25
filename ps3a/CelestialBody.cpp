9d72dc37c457852bfd2c21c789973a68d5b4481d
manasa7958 <manasapraveen.work@gmail.com>
2025-02-25 16:19:44 -0500

    Update CelestialBody.cpp

--- ps3a/CelestialBody.cpp
+++ ps3a/CelestialBody.cpp
@@ -34,9 +34,7 @@ bool CelestialBody::loadTexture(double universeRadius) {
   sprite.setPosition(screenX, screenY);
   return true;
 }
-\
-void CelestialBody::draw(sf::RenderTarget& window,
-                         sf::RenderStates states) const {
+void CelestialBody::draw(sf::RenderTarget& window, sf::RenderStates states) const {
   window.draw(sprite, states);
 }
 
