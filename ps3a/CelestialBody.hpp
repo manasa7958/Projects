f18dfea3079c5f985a1b93235c4e21a49999dc5d
manasa7958 <manasapraveen.work@gmail.com>
2025-02-25 14:54:09 -0500

    Update CelestialBody.hpp

--- ps3a/CelestialBody.hpp
+++ ps3a/CelestialBody.hpp
@@ -6,7 +6,7 @@
 #include <SFML/Graphics.hpp>
 
 namespace NB {
-class CelestialBody : public sf::Drawable {
+/*class CelestialBody : public sf::Drawable {
  public:
   explicit CelestialBody();
 
@@ -31,5 +31,32 @@ class CelestialBody : public sf::Drawable {
 };
 
 std::istream& operator>>(std::istream& in, CelestialBody& body);
-std::ostream& operator<<(std::ostream& out, const CelestialBody& body);
+std::ostream& operator<<(std::ostream& out, const CelestialBody& body);*/
+
+class CelestialBody : public sf::Drawable {
+ public:
+  explicit CelestialBody();
+
+  sf::Vector2f position() const { return pos; }
+  sf::Vector2f velocity() const { return vel; }
+  float mass() const { return m; }
+  std::string getImageFile() const { return imageFile; }  // ✅ Fix: Added getter
+
+  bool loadTexture(double universeRadius);
+
+ protected:
+  void draw(sf::RenderTarget& window, sf::RenderStates states) const override;
+
+ private:
+  sf::Vector2f pos;
+  sf::Vector2f vel;
+  float m;
+  std::string imageFile;
+  std::shared_ptr<sf::Texture> texture;
+  sf::Sprite sprite;
+
+  friend std::istream& operator>>(std::istream& in, CelestialBody& body);
+  friend std::ostream& operator<<(std::ostream& out, const CelestialBody& body);
+};
+
 }  // namespace NB
