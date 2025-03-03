071ed7190ed05769803c53cb20db9310af22c7b7
manasa7958 <manasapraveen.work@gmail.com>
2025-03-03 16:54:27 -0500

    Update main.cpp

--- ps3b/main.cpp
+++ ps3b/main.cpp
@@ -6,5 +6,18 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
+  NB::Universe universe;
+  std::cin >> universe;
+  sf::RenderWindow window(sf::VideoMode(800, 800), "The Solar System!");
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed)
+        window.close();
+    }
+    window.clear();
+    window.draw(universe);
+    window.display();
+  }
   return 0;
 }
