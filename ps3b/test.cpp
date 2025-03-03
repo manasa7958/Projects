8962e2766a0a364ea1d94859f74a05458621465e
manasa7958 <manasapraveen.work@gmail.com>
2025-03-03 16:58:32 -0500

    Update test.cpp

--- ps3b/test.cpp
+++ ps3b/test.cpp
@@ -27,18 +27,58 @@ BOOST_AUTO_TEST_CASE(testFlippedValues) {
   NB::CelestialBody body;
   input >> body;
 
-  BOOST_REQUIRE_CLOSE(body.position().x, 1.1111e+11, 0.001f);
-  BOOST_REQUIRE_CLOSE(body.position().y, 2.2222e+11, 0.001f);
-  BOOST_REQUIRE_CLOSE(body.velocity().x, 3.3333e+04, 0.001f);
-  BOOST_REQUIRE_CLOSE(body.velocity().y, 4.4444e+04, 0.001f);
-  BOOST_REQUIRE_CLOSE(body.mass(), 5.5555e+24, 0.001f);
+  double expected_x_pos = 1.1111e+11;
+  double expected_y_pos = 2.2222e+11;
+  double expected_x_vel = 3.3333e+04;
+  double expected_y_vel = 4.4444e+04;
+  double expected_mass = 5.5555e+24;
+  std::string expected_image = "flipped.gif";
+
+  BOOST_REQUIRE_CLOSE(body.position().x, expected_x_pos, 0.001f);
+  BOOST_REQUIRE_CLOSE(body.position().y, expected_y_pos, 0.001f);
+  BOOST_REQUIRE_CLOSE(body.velocity().x, expected_x_vel, 0.001f);
+  BOOST_REQUIRE_CLOSE(body.velocity().y, expected_y_vel, 0.001f);
+  BOOST_REQUIRE_CLOSE(body.mass(), expected_mass, 0.001f);
 }
 
-BOOST_AUTO_TEST_CASE(testNoAcceleration) {
+BOOST_AUTO_TEST_CASE(testFormatting) {
+  std::stringstream input(
+      "1.4960e+11 0.0000e+00 0.0000e+00 2.9800e+04 5.9740e+24 earth.gif");
+  NB::CelestialBody body;
+  input >> body;
+  std::stringstream output;
+  output << body;
+  NB::CelestialBody body2;
+  output >> body2;
+
+  BOOST_CHECK_CLOSE(body2.position().x, body.position().x, 0.001f);
+  BOOST_CHECK_CLOSE(body2.position().y, body.position().y, 0.001f);
+  BOOST_CHECK_CLOSE(body2.velocity().x, body.velocity().x, 0.001f);
+  BOOST_CHECK_CLOSE(body2.velocity().y, body.velocity().y, 0.001f);
+  BOOST_CHECK_CLOSE(body2.mass(), body.mass(), 0.001f);
+}
+
+BOOST_AUTO_TEST_CASE(testNumPlanets1) {
+    std::stringstream input("5 2.50e+11\n"
+         "1.4960e+11 0.0000e+00 0.0000e+00 2.9800e+04 5.9740e+24 earth.gif\n"
+         "2.2790e+11 0.0000e+00 0.0000e+00 2.4100e+04 6.4190e+23 mars.gif\n"
+         "5.7900e+10 0.0000e+00 0.0000e+00 4.7900e+04 3.3020e+23 mercury.gif\n"
+         "0.0000e+00 0.0000e+00 0.0000e+00 0.0000e+00 1.9890e+30 sun.gif\n"
+         "1.0820e+11 0.0000e+00 0.0000e+00 3.5000e+04 4.8690e+24 venus.gif\n");
+
+    NB::Universe universe;
+    input >> universe;
+
+    std::cout << "Number of planets read: " << universe.size() << std::endl;
+
+    BOOST_REQUIRE_EQUAL(universe.size(), 5);
+    BOOST_REQUIRE_NO_THROW(universe[0]);
+}
+/*BOOST_AUTO_TEST_CASE(testNoAcceleration) {
   std::stringstream input("1 1.0e+11\n0.0 0.0 0.0 0.0 1.0e+30 stationary.gif\n");
   NB::Universe universe;
   input >> universe;
   universe.step(1.0e+6);
   BOOST_TEST(universe[0].position().x == 0.0);
   BOOST_TEST(universe[0].position().y == 0.0);
-}
+}*/
