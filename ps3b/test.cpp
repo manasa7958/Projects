04e213ad2623adae1385fd55bcfbae44ecbfcbe0
manasa7958 <manasapraveen.work@gmail.com>
2025-03-03 17:24:29 -0500

    Update test.cpp

--- ps3b/test.cpp
+++ ps3b/test.cpp
@@ -78,16 +78,13 @@ BOOST_AUTO_TEST_CASE(testNumPlanets1) {
 BOOST_AUTO_TEST_CASE(testAntigravity) {
     std::stringstream input("2 1.0e+11\n"
         "0.0 0.0 0.0 0.0 1.0e+30 earth.gif\n"
-        "1.0e+11 0.0 0.0 0.0 1.0e+30 mars.gif\n");
+        "1.0e+11 0.0 0.0 0.0 1.0e+30 mars.gif\n"); 
 
     NB::Universe universe;
     input >> universe;
     universe.step(1.0e+6);
-
-    BOOST_TEST(universe[0].position().x == 0.0);
-    BOOST_TEST(universe[0].position().y == 0.0);
-    BOOST_TEST(universe[1].position().x == 1.0e+11);
-    BOOST_TEST(universe[1].position().y == 0.0);
+    BOOST_REQUIRE_CLOSE(universe[0].position().x, 0.0, 0.001);
+    BOOST_REQUIRE_CLOSE(universe[1].position().x, 1.0e+11, 0.001);
 }
 
 BOOST_AUTO_TEST_CASE(testNoAcceleration) {
