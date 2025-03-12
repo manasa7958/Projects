8a084798a4245d914ff8a694dd6dfe2f9cd5bc87
manasa7958 <manasapraveen.work@gmail.com>
2025-03-12 16:45:20 -0400

    Update CelestialBody.cpp

--- ps3b/CelestialBody.cpp
+++ ps3b/CelestialBody.cpp
@@ -7,45 +7,42 @@
 
 namespace NB {
 
-const double G = 6.67430e-11;  // Gravitational constant
-
+// Default Constructor
 CelestialBody::CelestialBody() : pos(0, 0), vel(0, 0), m(0), imageFile("") {}
 
-void CelestialBody::updateVelocity(const Vector2D& force, double dt) {
-  Vector2D acceleration(force.x / m, force.y / m);
-  vel.x += acceleration.x * dt;
-  vel.y += acceleration.y * dt;
+// Parameterized Constructor
+CelestialBody::CelestialBody(double x, double y, double vx, double vy, double mass, const std::string& imgFile)
+    : pos(x, y), vel(vx, vy), m(mass), imageFile(imgFile) {}
+
+// Apply force to update velocity
+void CelestialBody::applyForce(const Vector2D& force, double dt) {
+  if (m > 0) {
+    Vector2D acceleration = force / m;
+    vel = vel + acceleration * dt;
+  }
 }
 
+// Update position based on velocity
 void CelestialBody::updatePosition(double dt) {
-  pos.x += vel.x * dt;
-  pos.y += vel.y * dt;
+  pos = pos + vel * dt;
 }
 
-Vector2D CelestialBody::position() const {
-  return pos;
-}
-
-Vector2D CelestialBody::velocity() const {
-  return vel;
-}
-
-double CelestialBody::mass() const {
-  return m;
-}
-
+// Overloaded Input Operator
 std::istream& operator>>(std::istream& in, CelestialBody& body) {
   in >> body.pos.x >> body.pos.y >> body.vel.x >> body.vel.y >> body.m >> body.imageFile;
   return in;
 }
 
+// Overloaded Output Operator
 std::ostream& operator<<(std::ostream& out, const CelestialBody& body) {
-  out << std::scientific << std::setprecision(4) << std::uppercase << body.pos.x
-      << " " << body.pos.y << " " << body.vel.x << " " << body.vel.y << " "
+  out << std::scientific << std::setprecision(4) << std::uppercase
+      << body.pos.x << " " << body.pos.y << " " 
+      << body.vel.x << " " << body.vel.y << " " 
       << body.m << " " << body.imageFile << std::endl;
   return out;
 }
 
+// Load texture for graphical representation
 bool CelestialBody::loadTexture(double universeRadius) {
   texture = std::make_shared<sf::Texture>();
   if (!texture->loadFromFile(imageFile)) {
@@ -54,12 +51,13 @@ bool CelestialBody::loadTexture(double universeRadius) {
   }
   sprite.setTexture(*texture);
   const double SCALE_FACTOR = 400.0 / universeRadius;
-  float screenX = (pos.x * SCALE_FACTOR) + 400;  
+  float screenX = (pos.x * SCALE_FACTOR) + 400;  // Center in window
   float screenY = (pos.y * SCALE_FACTOR) + 400;
   sprite.setPosition(screenX, screenY);
   return true;
 }
 
+// Draw celestial body
 void CelestialBody::draw(sf::RenderTarget& window, sf::RenderStates states) const {
   window.draw(sprite, states);
 }
