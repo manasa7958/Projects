d94127c1e3ef0afa8b0ceece8cd300717fea0b45
manasa7958 <manasapraveen.work@gmail.com>
2025-03-12 16:16:04 -0400

    Update CelestialBody.hpp

--- ps3b/CelestialBody.hpp
+++ ps3b/CelestialBody.hpp
@@ -1,35 +1,39 @@
 // Copyright 2025 Manasa Praveen
-#pragma once
+// Copyright 2025 Manasa Praveen
+#ifndef CELESTIALBODY_HPP
+#define CELESTIALBODY_HPP
+
 #include <iostream>
-#include <string>
 #include <memory>
+#include <string>
+#include "Vector2D.hpp"
 #include <SFML/Graphics.hpp>
 
 namespace NB {
+
 class CelestialBody : public sf::Drawable {
  public:
-  explicit CelestialBody();
+  CelestialBody();
+  
+  void updateVelocity(const Vector2D& force, double dt);
+  void updatePosition(double dt);
 
-  sf::Vector2f position() const { return pos; }
-  sf::Vector2f velocity() const { return vel; }
-  float mass() const { return m; }
-  std::string getImageFile() const { return imageFile; }
+  friend std::istream& operator>>(std::istream& in, CelestialBody& body);
+  friend std::ostream& operator<<(std::ostream& out, const CelestialBody& body);
 
   bool loadTexture(double universeRadius);
-  void setVelocity(const sf::Vector2f& newVelocity) { vel = newVelocity; }
-  void setPosition(const sf::Vector2f& newPosition) { pos = newPosition; }
+
  protected:
   void draw(sf::RenderTarget& window, sf::RenderStates states) const override;
 
  private:
-  sf::Vector2f pos;
-  sf::Vector2f vel;
-  float m;
+  Vector2D pos, vel;
+  double m;
   std::string imageFile;
   std::shared_ptr<sf::Texture> texture;
   sf::Sprite sprite;
-  friend std::istream& operator>>(std::istream& in, CelestialBody& body);
-  friend std::ostream& operator<<(std::ostream& out, const CelestialBody& body);
 };
 
 }  // namespace NB
+
+#endif
