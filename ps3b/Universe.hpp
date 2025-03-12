1d24f86a752a68dd599e514d09d3d246d7fb78b7
manasa7958 <manasapraveen.work@gmail.com>
2025-03-12 16:23:00 -0400

    Update Universe.hpp

--- ps3b/Universe.hpp
+++ ps3b/Universe.hpp
@@ -9,17 +9,27 @@
 #include <SFML/Graphics.hpp>
 #include "CelestialBody.hpp"
 #include "Vector2D.hpp"
-
 namespace NB {
 
-class Universe {
+class Universe : public sf::Drawable {
  public:
   Universe();
   void step(double dt);
+  void addBody(std::shared_ptr<CelestialBody> body);
+  void clearBodies();
+  size_t size() const;
+  double radius() const;
+  void setRadius(double r);
+  const CelestialBody& operator[](size_t index) const;
+
+ protected:
+  void draw(sf::RenderTarget& window, sf::RenderStates states) const override;
 
  private:
   std::vector<std::shared_ptr<CelestialBody>> bodies;
   double universeRadius;
+  sf::Texture backgroundTexture;
+  sf::Sprite backgroundSprite;
 };
 
 }  // namespace NB
