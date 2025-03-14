879fc202574492ce8e663736725a43bf43b88dd4
manasa7958 <manasapraveen.work@gmail.com>
2025-03-13 22:29:06 -0400

    Update main.cpp

--- ps3b/main.cpp
+++ ps3b/main.cpp
@@ -5,35 +5,19 @@
 #include "Universe.hpp"
 #include <SFML/Graphics.hpp>
 
-int main(int argc, char* argv[]) {
-  if (argc < 3) {
-    std::cerr << "Usage: ./NBody <T> <dt> < planets.txt" << std::endl;
-    return 1;
-  }
-
-  double T = std::stod(argv[1]);
-  double dt = std::stod(argv[2]);
+int main() {
   NB::Universe universe;
   std::cin >> universe;
-
   sf::RenderWindow window(sf::VideoMode(800, 800), "The Solar System!");
-  double elapsedTime = 0.0;
-
-  while (window.isOpen() && elapsedTime < T) {
+  while (window.isOpen()) {
     sf::Event event;
     while (window.pollEvent(event)) {
       if (event.type == sf::Event::Closed)
         window.close();
     }
-
-    universe.step(dt);
-    elapsedTime += dt;
-
     window.clear();
     window.draw(universe);
     window.display();
   }
-
-  std::cout << universe;  // Print final state of universe
   return 0;
 }
