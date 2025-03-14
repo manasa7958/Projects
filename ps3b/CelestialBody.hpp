6e5099a165e9ba314da7c8ba7cd457c6a3b9d39e
manasa7958 <manasapraveen.work@gmail.com>
2025-03-13 22:27:42 -0400

    Update CelestialBody.hpp

--- ps3b/CelestialBody.hpp
+++ ps3b/CelestialBody.hpp
@@ -1,46 +1,35 @@
 // Copyright 2025 Manasa Praveen
-#ifndef CELESTIALBODY_HPP
-#define CELESTIALBODY_HPP
-
+#pragma once
 #include <iostream>
-#include <memory>
 #include <string>
-#include "Vector2D.hpp"
+#include <memory>
 #include <SFML/Graphics.hpp>
 
 namespace NB {
-
 class CelestialBody : public sf::Drawable {
  public:
-  CelestialBody(double x, double y, double vx, double vy, double mass, const std::string& imgFile);
+  explicit CelestialBody();
 
-  const Vector2D& position() const { return pos; }
-  const Vector2D& velocity() const { return vel; }
-  double mass() const { return m; }
-
-  void setVelocity(const Vector2D& newVel) { vel = newVel; }
-  void setPosition(const Vector2D& newPos) { pos = newPos; }
-
-  void applyForce(const Vector2D& force, double dt);
-  void updatePosition(double dt);
+  sf::Vector2f position() const { return pos; }
+  sf::Vector2f velocity() const { return vel; }
+  float mass() const { return m; }
+  std::string getImageFile() const { return imageFile; }
 
   bool loadTexture(double universeRadius);
-  
-  friend std::istream& operator>>(std::istream& in, CelestialBody& body);
-  friend std::ostream& operator<<(std::ostream& out, const CelestialBody& body);
-
+  void setVelocity(const sf::Vector2f& newVelocity) { vel = newVelocity; }
+  void setPosition(const sf::Vector2f& newPosition) { pos = newPosition; }
  protected:
   void draw(sf::RenderTarget& window, sf::RenderStates states) const override;
 
  private:
-  Vector2D pos;
-  Vector2D vel;
-  double m;
+  sf::Vector2f pos;
+  sf::Vector2f vel;
+  float m;
   std::string imageFile;
   std::shared_ptr<sf::Texture> texture;
   sf::Sprite sprite;
+  friend std::istream& operator>>(std::istream& in, CelestialBody& body);
+  friend std::ostream& operator<<(std::ostream& out, const CelestialBody& body);
 };
 
 }  // namespace NB
-
-#endif  // CELESTIALBODY_HPP
