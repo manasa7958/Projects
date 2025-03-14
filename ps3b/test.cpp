80a2da30bd51c4c622588f8f1ea3d9e615bd4835
manasa7958 <manasapraveen.work@gmail.com>
2025-03-13 22:20:24 -0400

    Update test.cpp

--- ps3b/test.cpp
+++ ps3b/test.cpp
@@ -16,39 +16,38 @@
 using namespace NB;
 
 BOOST_AUTO_TEST_CASE(test_celestial_body_initialization) {
-    CelestialBody body(1.0e24, 1.0e11, 2.0e11, 3.0e3, 4.0e3, "earth.gif");
-    BOOST_CHECK_CLOSE(body.getMass(), 1.0e24, 1e-6);
-    BOOST_CHECK_CLOSE(body.getPosition().x, 1.0e11, 1e-6);
-    BOOST_CHECK_CLOSE(body.getPosition().y, 2.0e11, 1e-6);
-    BOOST_CHECK_CLOSE(body.getVelocity().x, 3.0e3, 1e-6);
-    BOOST_CHECK_CLOSE(body.getVelocity().y, 4.0e3, 1e-6);
-    BOOST_CHECK_EQUAL(body.getImage(), "earth.gif");
+    CelestialBody body(1.0e11, 2.0e11, 3.0e3, 4.0e3, 1.0e24, "earth.gif");
+    BOOST_CHECK_CLOSE(body.mass(), 1.0e24, 1e-6);
+    BOOST_CHECK_CLOSE(body.position().x, 1.0e11, 1e-6);
+    BOOST_CHECK_CLOSE(body.position().y, 2.0e11, 1e-6);
+    BOOST_CHECK_CLOSE(body.velocity().x, 3.0e3, 1e-6);
+    BOOST_CHECK_CLOSE(body.velocity().y, 4.0e3, 1e-6);
 }
 
 BOOST_AUTO_TEST_CASE(test_universe_step) {
     Universe universe;
-    universe.addBody(std::make_shared<CelestialBody>(1.0e24, 0.0, 0.0, 0.0, 0.0, "sun.gif"));
-    universe.addBody(std::make_shared<CelestialBody>(5.0e23, 1.0e11, 0.0, 0.0, 3.0e4, "mars.gif"));
+    universe.addBody(std::make_shared<CelestialBody>(0.0, 0.0, 0.0, 0.0, 1.0e24, "sun.gif"));
+    universe.addBody(std::make_shared<CelestialBody>(1.0e11, 0.0, 0.0, 3.0e4, 5.0e23, "mars.gif"));
     
-    double initial_x = universe.getBodies()[1]->getPosition().x;
-    double initial_y = universe.getBodies()[1]->getPosition().y;
+    double initial_x = universe[1].position().x;
+    double initial_y = universe[1].position().y;
     
     universe.step(25000.0);
     
-    BOOST_CHECK(universe.getBodies()[1]->getPosition().x != initial_x);
-    BOOST_CHECK(universe.getBodies()[1]->getPosition().y != initial_y);
+    BOOST_CHECK(universe[1].position().x != initial_x);
+    BOOST_CHECK(universe[1].position().y != initial_y);
 }
 
 BOOST_AUTO_TEST_CASE(test_velocity_update) {
     Universe universe;
-    auto planet = std::make_shared<CelestialBody>(5.0e23, 1.0e11, 0.0, 0.0, 3.0e4, "venus.gif");
+    auto planet = std::make_shared<CelestialBody>(1.0e11, 0.0, 0.0, 3.0e4, 5.0e23, "venus.gif");
     universe.addBody(planet);
     
-    double initial_vx = planet->getVelocity().x;
-    double initial_vy = planet->getVelocity().y;
+    double initial_vx = planet->velocity().x;
+    double initial_vy = planet->velocity().y;
     
     universe.step(25000.0);
     
-    BOOST_CHECK(planet->getVelocity().x != initial_vx);
-    BOOST_CHECK(planet->getVelocity().y != initial_vy);
+    BOOST_CHECK(planet->velocity().x != initial_vx);
+    BOOST_CHECK(planet->velocity().y != initial_vy);
 }
