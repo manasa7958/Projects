6693113f5ad5f2329e5c5e3b731cf7c5ca1534d0
manasa7958 <manasapraveen.work@gmail.com>
2025-03-13 22:04:47 -0400

    Update CelestialBody.cpp

--- ps3b/CelestialBody.cpp
+++ ps3b/CelestialBody.cpp
@@ -7,14 +7,11 @@
 
 namespace NB {
 
-// Default Constructor
 CelestialBody::CelestialBody() : pos(0, 0), vel(0, 0), m(0), imageFile("") {}
 
-// Parameterized Constructor
 CelestialBody::CelestialBody(double x, double y, double vx, double vy, double mass, const std::string& imgFile)
     : pos(x, y), vel(vx, vy), m(mass), imageFile(imgFile) {}
 
-// Apply force to update velocity
 void CelestialBody::applyForce(const Vector2D& force, double dt) {
   if (m > 0) {
     Vector2D acceleration = force / m;
@@ -22,18 +19,15 @@ void CelestialBody::applyForce(const Vector2D& force, double dt) {
   }
 }
 
-// Update position based on velocity
 void CelestialBody::updatePosition(double dt) {
   pos = pos + vel * dt;
 }
 
-// Overloaded Input Operator
 std::istream& operator>>(std::istream& in, CelestialBody& body) {
   in >> body.pos.x >> body.pos.y >> body.vel.x >> body.vel.y >> body.m >> body.imageFile;
   return in;
 }
 
-// Overloaded Output Operator
 std::ostream& operator<<(std::ostream& out, const CelestialBody& body) {
   out << std::scientific << std::setprecision(4) << std::uppercase
       << body.pos.x << " " << body.pos.y << " " 
@@ -42,7 +36,6 @@ std::ostream& operator<<(std::ostream& out, const CelestialBody& body) {
   return out;
 }
 
-// Load texture for graphical representation
 bool CelestialBody::loadTexture(double universeRadius) {
   texture = std::make_shared<sf::Texture>();
   if (!texture->loadFromFile(imageFile)) {
@@ -51,13 +44,12 @@ bool CelestialBody::loadTexture(double universeRadius) {
   }
   sprite.setTexture(*texture);
   const double SCALE_FACTOR = 400.0 / universeRadius;
-  float screenX = (pos.x * SCALE_FACTOR) + 400;  // Center in window
+  float screenX = (pos.x * SCALE_FACTOR) + 400;
   float screenY = (pos.y * SCALE_FACTOR) + 400;
   sprite.setPosition(screenX, screenY);
   return true;
 }
 
-// Draw celestial body
 void CelestialBody::draw(sf::RenderTarget& window, sf::RenderStates states) const {
   window.draw(sprite, states);
 }
