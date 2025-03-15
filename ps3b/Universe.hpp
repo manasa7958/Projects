fe6316d2bd5ef50f78ce9f9545be58d99d24d84e
manasa7958 <manasapraveen.work@gmail.com>
2025-03-15 19:42:04 -0400

    Update Universe.hpp

--- ps3b/Universe.hpp
+++ ps3b/Universe.hpp
@@ -1,27 +1,33 @@
+// Copyright 2025 Manasa Praveen
 #pragma once
-
 #include <iostream>
-
+#include <memory>
+#include <vector>
+#include <string>
 #include <SFML/Graphics.hpp>
-
 #include "CelestialBody.hpp"
 
 namespace NB {
-class Universe: public sf::Drawable {
+class Universe : public sf::Drawable {
  public:
-    Universe(); // Required
-    explicit Universe(const std::string& filename);  // Optional
+  Universe();                                       // Required
+  explicit Universe(const std::string& filename);   // Optional
+  size_t size() const;                              // Optional
+  double radius() const;                            // Optional
+  const CelestialBody& operator[](size_t i) const;  // Optional
+  void step(double dt);
+  void setRadius(double r) { universeRadius = r; }
+  void addBody(std::shared_ptr<CelestialBody> body) { bodies.push_back(body); }
+  void clearBodies() { bodies.clear(); }
 
-    size_t size() const; // Optional
-    double radius() const; // Optional
-
-    const CelestialBody& operator[](size_t i) const; // Optional
-
-    void step(double dt); // Implemented in part b, behavior for part a is undefined
  protected:
-    void draw(sf::RenderTarget& window, sf::RenderStates states) const override; // From sf::Drawable
+  void draw(sf::RenderTarget& window,
+            sf::RenderStates states) const override;  // From sf::Drawable
  private:
-    // Fields and helper functions go here
+  double universeRadius;  // Radius variable
+  std::vector<std::shared_ptr<CelestialBody>> bodies;  // Smart pointer
+  sf::Texture backgroundTexture;
+  sf::Sprite backgroundSprite;
 };
 
 std::istream& operator>>(std::istream& is, Universe& uni);
