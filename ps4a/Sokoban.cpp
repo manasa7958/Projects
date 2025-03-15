bd9a27928596dfd3bcb99d47a7d04f9bb0300435
manasa7958 <manasapraveen.work@gmail.com>
2025-03-15 13:59:36 -0400

    Update Sokoban.cpp

--- ps4a/Sokoban.cpp
+++ ps4a/Sokoban.cpp
@@ -1,7 +1,11 @@
 //Copyright Manasa praveen 2025
-#include "Sokoban.hpp"
+#pragma once
+
+#include <iostream>
 #include <fstream>
 #include <stdexcept>
+#include "Sokoban.hpp"
+#include <SFML/Graphics.hpp>
 
 namespace SB {
 
@@ -13,6 +17,23 @@ Sokoban::Sokoban(const std::string& filename) {
         throw std::runtime_error("Unable to open file");
     }
     file >> *this;
+
+    // Load textures
+    if (!wallTexture.loadFromFile("sokoban/block_06.png")) {
+        throw std::runtime_error("Failed to load wall texture");
+    }
+    if (!boxTexture.loadFromFile("sokoban/crate_03.png")) {
+        throw std::runtime_error("Failed to load box texture");
+    }
+    if (!groundTexture.loadFromFile("sokoban/ground_01.png")) {
+        throw std::runtime_error("Failed to load ground texture");
+    }
+    if (!storageTexture.loadFromFile("sokoban/ground_04.png")) {
+        throw std::runtime_error("Failed to load storage texture");
+    }
+    if (!playerTexture.loadFromFile("sokoban/player_05.png")) {
+        throw std::runtime_error("Failed to load player texture");
+    }
 }
 
 unsigned int Sokoban::width() const {
@@ -28,7 +49,6 @@ sf::Vector2u Sokoban::playerLoc() const {
 }
 
 bool Sokoban::isWon() const {
-    // Check if all storage locations have boxes
     for (const auto& row : board) {
         if (row.find('a') != std::string::npos) {
             return false;
@@ -38,15 +58,37 @@ bool Sokoban::isWon() const {
 }
 
 void Sokoban::movePlayer(Direction dir) {
-    // Placeholder for movement logic (to be implemented in Part B)
+    // Placeholder for movement logic
 }
 
 void Sokoban::reset() {
-    // Reset game state logic (to be implemented)
+    // Reset game state logic
 }
 
 void Sokoban::draw(sf::RenderTarget& target, sf::RenderStates states) const {
-    // Rendering logic will be added here
+    for (unsigned int y = 0; y < boardHeight; ++y) {
+        for (unsigned int x = 0; x < boardWidth; ++x) {
+            sf::Sprite sprite;
+            char tile = board[y][x];
+            
+            if (tile == '#') {
+                sprite.setTexture(wallTexture);
+            } else if (tile == 'A') {
+                sprite.setTexture(boxTexture);
+            } else if (tile == '.') {
+                sprite.setTexture(groundTexture);
+            } else if (tile == 'a') {
+                sprite.setTexture(storageTexture);
+            } else if (tile == '@') {
+                sprite.setTexture(playerTexture);
+            } else {
+                sprite.setTexture(groundTexture); // Default to ground
+            }
+            
+            sprite.setPosition(x * TILE_SIZE, y * TILE_SIZE);
+            target.draw(sprite, states);
+        }
+    }
 }
 
 std::ostream& operator<<(std::ostream& out, const Sokoban& s) {
@@ -61,7 +103,7 @@ std::istream& operator>>(std::istream& in, Sokoban& s) {
     in >> s.boardHeight >> s.boardWidth;
     s.board.clear();
     s.board.resize(s.boardHeight);
-    in.ignore(); // Ignore newline
+    in.ignore();
     
     for (unsigned int i = 0; i < s.boardHeight; ++i) {
         std::getline(in, s.board[i]);
