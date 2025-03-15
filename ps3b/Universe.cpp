16c471c594c9eee858322ab27159d6705dae7eda
manasa7958 <manasapraveen.work@gmail.com>
2025-03-15 19:42:34 -0400

    Create Universe.cpp

---
+++ ps3b/Universe.cpp
@@ -0,0 +1,71 @@
+// Copyright 2025 Manasa Praveen
+#include <iostream>
+#include <memory>
+#include "Universe.hpp"
+
+namespace NB {
+Universe::Universe() : universeRadius(0) {
+    if (!backgroundTexture.loadFromFile("background.jpg")) {
+        std::cerr << "Failed to load background image" << std::endl;
+    } else {
+        backgroundSprite.setTexture(backgroundTexture);
+        sf::Vector2u textureSize = backgroundTexture.getSize();
+        sf::Vector2u windowSize(800, 800);
+        float scaleX = static_cast<float>(windowSize.x) / textureSize.x;
+        float scaleY = static_cast<float>(windowSize.y) / textureSize.y;
+        backgroundSprite.setScale(scaleX, scaleY);
+    }
+}
+
+std::istream& operator>>(std::istream& in, Universe& universe) {
+    size_t n;
+    double radius;
+    in >> n >> radius;
+    universe.setRadius(radius);
+    universe.clearBodies();
+
+    if (n == 0) return in;
+    for (size_t i = 0; i < n; ++i) {
+        auto body = std::make_shared<CelestialBody>();
+        in >> *body;
+        if (!body->loadTexture(radius)) {
+          std::cerr << "Failed to load texture" << std::endl;
+        }
+        universe.addBody(body);
+    }
+    return in;
+}
+
+std::ostream& operator<<(std::ostream& out, const Universe& universe) {
+  out << universe.size() << " " << universe.radius() << "\n";
+  for (size_t i = 0; i < universe.size(); ++i) {
+    out << universe[i];
+  }
+  return out;
+}
+
+size_t Universe::size() const {
+  return bodies.size();
+}
+
+double Universe::radius() const {
+  return universeRadius;
+}
+
+const CelestialBody& Universe::operator[](size_t index) const {
+  if (index >= bodies.size()) {
+    throw std::out_of_range("Index out of range");
+  }
+  return *bodies[index];
+}
+
+void Universe::draw(sf::RenderTarget& window, sf::RenderStates states) const {
+    window.draw(backgroundSprite);
+    if (bodies.empty()) {
+    std::cerr << "No celestial bodies found!";
+    }
+    for (const auto& body : bodies) {
+        window.draw(*body, states);
+    }
+}
+}  // namespace NB
