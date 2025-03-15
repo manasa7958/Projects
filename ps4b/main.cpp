69d68c57380ec891df7b21bf64cdb0619c58a743
manasa7958 <manasapraveen.work@gmail.com>
2025-03-15 18:48:44 -0400

    Update main.cpp

--- ps4b/main.cpp
+++ ps4b/main.cpp
@@ -12,32 +12,34 @@ int main(int argc, char* argv[]) {
     SB::Sokoban game(argv[1]);
     sf::RenderWindow window(sf::VideoMode(game.width() * SB::Sokoban::TILE_SIZE,
                             game.height() * SB::Sokoban::TILE_SIZE), "Sokoban");
-
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
-        
+    
             if (event.type == sf::Event::KeyPressed) {
-                if (event.key.code == sf::Keyboard::W || event.key.code == sf::Keyboard::Up) {
-                    game.movePlayer(SB::Direction::Up);
-                } else if (event.key.code == sf::Keyboard::S || event.key.code == sf::Keyboard::Down) {
-                    game.movePlayer(SB::Direction::Down);
-                } else if (event.key.code == sf::Keyboard::A || event.key.code == sf::Keyboard::Left) {
-                    game.movePlayer(SB::Direction::Left);
-                } else if (event.key.code == sf::Keyboard::D || event.key.code == sf::Keyboard::Right) {
-                    game.movePlayer(SB::Direction::Right);
-                } else if (event.key.code == sf::Keyboard::R) {  // Reset game
-                    game.reset();
+                if (!game.isWon()) {  // Prevent movement after winning
+                    if (event.key.code == sf::Keyboard::W || event.key.code == sf::Keyboard::Up) {
+                        game.movePlayer(SB::Direction::Up);
+                    } else if (event.key.code == sf::Keyboard::S || event.key.code == sf::Keyboard::Down) {
+                        game.movePlayer(SB::Direction::Down);
+                    } else if (event.key.code == sf::Keyboard::A || event.key.code == sf::Keyboard::Left) {
+                        game.movePlayer(SB::Direction::Left);
+                    } else if (event.key.code == sf::Keyboard::D || event.key.code == sf::Keyboard::Right) {
+                        game.movePlayer(SB::Direction::Right);
+                    }
+                }
+                if (event.key.code == sf::Keyboard::R) {
+                    game.reset();  // Properly reset game
                 }
             }
         }
+    
         window.clear();
         window.draw(game);
         window.display();
     }
-
     return 0;
 }
