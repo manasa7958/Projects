261852f60268214761183abe3f952eec55d1d896
manasa7958 <manasapraveen.work@gmail.com>
2025-03-15 20:25:55 -0400

    Update test.cpp

--- ps3b/test.cpp
+++ ps3b/test.cpp
@@ -4,7 +4,6 @@
 #include <iostream>
 #include <sstream>
 #include <string>
-
 #include "CelestialBody.hpp"
 #include "Universe.hpp"
 
@@ -17,28 +16,45 @@ BOOST_AUTO_TEST_CASE(testEmptyUniverse) {
   std::stringstream input("0 1.0e+11\n");
   NB::Universe universe;
   input >> universe;
-
   BOOST_TEST(universe.size() == 0);
 }
 
-BOOST_AUTO_TEST_CASE(testFlippedValues) {
+BOOST_AUTO_TEST_CASE(testNoAcceleration) {
   std::stringstream input(
-      "1.1111e+11 2.2222e+11 3.3333e+04 4.4444e+04 5.5555e+24 flipped.gif");
-  NB::CelestialBody body;
-  input >> body;
+      "1\n"
+      "1.0e+11\n"
+      "0.0000e+00 0.0000e+00 0.0000e+00 0.0000e+00 5.9740e+24 earth.gif\n");
+  
+  NB::Universe universe;
+  input >> universe;
+  
+  double initial_x = universe[0].position().x;
+  double initial_y = universe[0].position().y;
+  universe.step(10000.0);
+  
+  BOOST_TEST(universe[0].position().x == initial_x);
+  BOOST_TEST(universe[0].position().y == initial_y);
+}
 
-  double expected_x_pos = 1.1111e+11;
-  double expected_y_pos = 2.2222e+11;
-  double expected_x_vel = 3.3333e+04;
-  double expected_y_vel = 4.4444e+04;
-  double expected_mass = 5.5555e+24;
-  std::string expected_image = "flipped.gif";
+BOOST_AUTO_TEST_CASE(testSinglePlanetNoMotion) {
+  std::stringstream input("1 1.0e+11\n"
+                          "0.0 0.0 0.0 0.0 5.974e+24 earth.gif\n");
+  NB::Universe universe;
+  input >> universe;
+  universe.step(1.0e6);
+  BOOST_TEST(universe[0].position().x == 0.0);
+  BOOST_TEST(universe[0].position().y == 0.0);
+}
 
-  BOOST_REQUIRE_CLOSE(body.position().x, expected_x_pos, 0.001f);
-  BOOST_REQUIRE_CLOSE(body.position().y, expected_y_pos, 0.001f);
-  BOOST_REQUIRE_CLOSE(body.velocity().x, expected_x_vel, 0.001f);
-  BOOST_REQUIRE_CLOSE(body.velocity().y, expected_y_vel, 0.001f);
-  BOOST_REQUIRE_CLOSE(body.mass(), expected_mass, 0.001f);
+BOOST_AUTO_TEST_CASE(testTwoBodiesAttract) {
+  std::stringstream input("2 1.0e+11\n"
+                          "-1.0e+11 0.0 0.0 0.0 5.974e+24 earth.gif\n"
+                          "1.0e+11 0.0 0.0 0.0 5.974e+24 earth.gif\n");
+  NB::Universe universe;
+  input >> universe;
+  universe.step(1.0e6);
+  BOOST_TEST(universe[0].position().x > -1.0e+11);
+  BOOST_TEST(universe[1].position().x < 1.0e+11);
 }
 
 BOOST_AUTO_TEST_CASE(testFormatting) {
@@ -58,19 +74,21 @@ BOOST_AUTO_TEST_CASE(testFormatting) {
   BOOST_CHECK_CLOSE(body2.mass(), body.mass(), 0.001f);
 }
 
-BOOST_AUTO_TEST_CASE(testNumPlanets1) {
-    std::stringstream input("5 2.50e+11\n"
+BOOST_AUTO_TEST_CASE(testMultipleSteps) {
+  std::stringstream input("5 2.50e+11\n"
          "1.4960e+11 0.0000e+00 0.0000e+00 2.9800e+04 5.9740e+24 earth.gif\n"
          "2.2790e+11 0.0000e+00 0.0000e+00 2.4100e+04 6.4190e+23 mars.gif\n"
          "5.7900e+10 0.0000e+00 0.0000e+00 4.7900e+04 3.3020e+23 mercury.gif\n"
          "0.0000e+00 0.0000e+00 0.0000e+00 0.0000e+00 1.9890e+30 sun.gif\n"
          "1.0820e+11 0.0000e+00 0.0000e+00 3.5000e+04 4.8690e+24 venus.gif\n");
 
-    NB::Universe universe;
-    input >> universe;
+  NB::Universe universe;
+  input >> universe;
 
-    std::cout << "Number of planets read: " << universe.size() << std::endl;
+  for (int i = 0; i < 100; ++i) {
+    universe.step(1.0e6);
+  }
 
-    BOOST_REQUIRE_EQUAL(universe.size(), 5);
-    BOOST_REQUIRE_NO_THROW(universe[0]);
+  BOOST_REQUIRE_NO_THROW(universe[0]);
+  BOOST_TEST(universe.size() == 5);
 }
