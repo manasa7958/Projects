d47649a6f72db289cf7507a95dfb7f8dd78dff85
manasa7958 <manasapraveen.work@gmail.com>
2025-03-17 17:51:08 -0400

    Update CelestialBody.cpp

--- ps3b/CelestialBody.cpp
+++ ps3b/CelestialBody.cpp
@@ -7,34 +7,32 @@
 
 namespace NB {
 
-CelestialBody::CelestialBody() : pos(0, 0), vel(0, 0), m(0), imageFile("") {}
+CelestialBody::CelestialBody() 
+    : pos(0, 0), vel(0, 0), m(0), imageFile("body.gif"), texture(std::make_shared<sf::Texture>()) {}
 
 std::istream& operator>>(std::istream& in, CelestialBody& body) {
-  in >> body.pos.x >> body.pos.y >> body.vel.x >> body.vel.y >> body.m >> body.imageFile;
-  return in;
+    in >> body.pos.x >> body.pos.y >> body.vel.x >> body.vel.y >> body.m >> body.imageFile;
+
+    if (!body.texture->loadFromFile(body.imageFile)) {
+        std::cerr << "Failed to load texture: " << body.imageFile << std::endl;
+    } else {
+        body.sprite.setTexture(*body.texture);
+        body.sprite.setOrigin(body.texture->getSize().x / 2, body.texture->getSize().y / 2);
+    }
+
+    return in;
 }
 
 std::ostream& operator<<(std::ostream& out, const CelestialBody& body) {
-  out << std::scientific << std::setprecision(4) << std::uppercase
-      << body.pos.x << " " << body.pos.y << " "
-      << body.vel.x << " " << body.vel.y << " "
-      << body.m << " " << body.imageFile << std::endl;
-  return out;
-}
-
-bool CelestialBody::loadTexture(double universeRadius) {
-  texture = std::make_shared<sf::Texture>();
-  if (!texture->loadFromFile(imageFile)) {
-    std::cerr << "Failed to load texture " << imageFile << std::endl;
-    return false;
-  }
-  sprite.setTexture(*texture);
-  sprite.setOrigin(texture->getSize().x / 2, texture->getSize().y / 2);
-  return true;
+    out << std::scientific << std::setprecision(4) << std::uppercase
+        << body.pos.x << " " << body.pos.y << " "
+        << body.vel.x << " " << body.vel.y << " "
+        << body.m << " " << body.imageFile << std::endl;
+    return out;
 }
 
 void CelestialBody::draw(sf::RenderTarget& window, sf::RenderStates states) const {
-  window.draw(sprite, states);
+    window.draw(sprite, states);
 }
 
-} // namespace NB
+}  // namespace NB
