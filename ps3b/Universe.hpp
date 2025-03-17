71b4904e724a87608d373a166d3ac4ce025f3b87
manasa7958 <manasapraveen.work@gmail.com>
2025-03-17 18:00:04 -0400

    Update Universe.hpp

--- ps3b/Universe.hpp
+++ ps3b/Universe.hpp
@@ -3,33 +3,29 @@
 #include <iostream>
 #include <memory>
 #include <vector>
-#include <string>
 #include <SFML/Graphics.hpp>
 #include "CelestialBody.hpp"
 
 namespace NB {
 class Universe : public sf::Drawable {
  public:
-  Universe();                                       // Required
-  explicit Universe(const std::string& filename);   // Optional
-  size_t size() const;                              // Optional
-  double radius() const;                            // Optional
-  const CelestialBody& operator[](size_t i) const;  // Optional
+  Universe();
   void step(double dt);
   void setRadius(double r) { universeRadius = r; }
   void addBody(std::shared_ptr<CelestialBody> body) { bodies.push_back(body); }
   void clearBodies() { bodies.clear(); }
+  size_t size() const { return bodies.size(); }
+  double radius() const { return universeRadius; }
 
  protected:
-  void draw(sf::RenderTarget& window,
-            sf::RenderStates states) const override;  // From sf::Drawable
+  void draw(sf::RenderTarget& window, sf::RenderStates states) const override;
+
  private:
-  double universeRadius;  // Radius variable
-  std::vector<std::shared_ptr<CelestialBody>> bodies;  // Smart pointer
+  double universeRadius;
+  std::vector<std::shared_ptr<CelestialBody>> bodies;
   sf::Texture backgroundTexture;
   sf::Sprite backgroundSprite;
 };
 
 std::istream& operator>>(std::istream& is, Universe& uni);
-std::ostream& operator<<(std::ostream& os, const Universe& uni);
 }  // namespace NB
