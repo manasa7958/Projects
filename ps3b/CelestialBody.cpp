94d476ca590766ecd98084f5c542a63124220819
manasa7958 <manasapraveen.work@gmail.com>
2025-03-17 18:17:26 -0400

    Update CelestialBody.cpp

--- ps3b/CelestialBody.cpp
+++ ps3b/CelestialBody.cpp
@@ -4,32 +4,30 @@
 #include <memory>
 #include <cmath>
 #include "CelestialBody.hpp"
-#include <SFML/Graphics.hpp>
 
 namespace NB {
 
 CelestialBody::CelestialBody() 
     : pos(0, 0), vel(0, 0), m(0), imageFile("body.gif"), texture(std::make_shared<sf::Texture>()) {}
 
+bool CelestialBody::loadTexture(double universeRadius) {
+    if (!texture->loadFromFile(imageFile)) {
+        std::cerr << "Failed to load texture: " << imageFile << std::endl;
+        return false;
+    }
+    sprite.setTexture(*texture);
+    sprite.setOrigin(texture->getSize().x / 2, texture->getSize().y / 2);
+    return true;
+}
+
 std::istream& operator>>(std::istream& in, CelestialBody& body) {
     in >> body.pos.x >> body.pos.y >> body.vel.x >> body.vel.y >> body.m >> body.imageFile;
 
-    if (!body.texture->loadFromFile(body.imageFile)) {
-        std::cerr << "Failed to load texture: " << body.imageFile << std::endl;
-    } else {
-        body.sprite.setTexture(*body.texture);
-        body.sprite.setOrigin(body.texture->getSize().x / 2, body.texture->getSize().y / 2);
+    if (!body.loadTexture(1.0)) {  // Call texture loader
+        std::cerr << "Failed to load texture for " << body.imageFile << std::endl;
     }
 
     return in;
-}
-
-std::ostream& operator<<(std::ostream& out, const CelestialBody& body) {
-    out << std::scientific << std::setprecision(4) << std::uppercase
-        << body.pos.x << " " << body.pos.y << " "
-        << body.vel.x << " " << body.vel.y << " "
-        << body.m << " " << body.imageFile << std::endl;
-    return out;
 }
 
 void CelestialBody::draw(sf::RenderTarget& window, sf::RenderStates states) const {
