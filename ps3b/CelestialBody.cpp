942892fe601ae3baddf29ecc869ac89e9c124960
manasa7958 <manasapraveen.work@gmail.com>
2025-03-17 19:54:22 -0400

    Update CelestialBody.cpp

--- ps3b/CelestialBody.cpp
+++ ps3b/CelestialBody.cpp
@@ -8,15 +8,16 @@
 namespace NB {
 
 CelestialBody::CelestialBody() 
-    : pos(0, 0), vel(0, 0), m(0), imageFile("body.gif"), texture(std::make_shared<sf::Texture>()) {}
+    : pos(0, 0), vel(0, 0), m(0), imageFile("body.gif"), 
+      texture(std::make_shared<sf::Texture>()), sprite(std::make_shared<sf::Sprite>()) {}
 
 bool CelestialBody::loadTexture(double universeRadius) {
     if (!texture->loadFromFile(imageFile)) {
         std::cerr << "Failed to load texture: " << imageFile << std::endl;
         return false;
     }
-    sprite.setTexture(*texture);
-    sprite.setOrigin(texture->getSize().x / 2, texture->getSize().y / 2);
+    sprite->setTexture(*texture);
+    sprite->setOrigin(texture->getSize().x / 2, texture->getSize().y / 2);
     return true;
 }
 
@@ -30,8 +31,20 @@ std::istream& operator>>(std::istream& in, CelestialBody& body) {
     return in;
 }
 
+std::ostream& operator<<(std::ostream& out, const CelestialBody& body) {
+    out << std::scientific << std::setprecision(4) << std::uppercase
+        << body.pos.x << " " << body.pos.y << " "
+        << body.vel.x << " " << body.vel.y << " "
+        << body.m << " " << body.imageFile;
+    return out;
+}
+
 void CelestialBody::draw(sf::RenderTarget& window, sf::RenderStates states) const {
-    window.draw(sprite, states);
+    if (sprite) {
+        window.draw(*sprite, states);
+    } else {
+        std::cerr << "Warning: Sprite is null, cannot draw." << std::endl;
+    }
 }
 
 }  // namespace NB
