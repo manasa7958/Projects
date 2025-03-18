d1c226596fcfe31aeb4e73a10f9127ebf78dad28
manasa7958 <manasapraveen.work@gmail.com>
2025-03-18 18:15:38 -0400

    Update CelestialBody.hpp

--- ps3b?/CelestialBody.hpp
+++ ps3b?/CelestialBody.hpp
@@ -1,4 +1,4 @@
-// CelestialBody.hpp - Fixed Version
+/*// CelestialBody.hpp - Fixed Version
 #pragma once
 #include <iostream>
 #include <SFML/Graphics.hpp>
@@ -30,4 +30,38 @@ class CelestialBody: public sf::Drawable {
     friend std::istream& operator>>(std::istream& is, CelestialBody& body);
     friend std::ostream& operator<<(std::ostream& os, const CelestialBody& body);
 };
+}  // namespace NB*/
+// CelestialBody.hpp - Updated to use sf::Vector2f for position and velocity
+#pragma once
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+namespace NB {
+class CelestialBody: public sf::Drawable {
+ public:
+    explicit CelestialBody(); // Required
+
+    sf::Vector2f position() const; // Optional
+    sf::Vector2f velocity() const; // Optional
+    float mass() const; // Optional
+
+    void setPosition(const sf::Vector2f& pos);
+    void setVelocity(const sf::Vector2f& vel);
+    bool getSprite(sf::Sprite& outSprite) const;
+
+ protected:
+    void draw(sf::RenderTarget& window, sf::RenderStates states) const override; // From sf::Drawable
+
+ private:
+    sf::Vector2f pos_;
+    sf::Vector2f vel_;
+    float weight_;
+    std::string textureFile_;
+    std::shared_ptr<sf::Texture> texture_;
+    std::shared_ptr<sf::Sprite> sprite_;
+
+    friend std::istream& operator>>(std::istream& is, CelestialBody& body);
+    friend std::ostream& operator<<(std::ostream& os, const CelestialBody& body);
+};
 }  // namespace NB
+
