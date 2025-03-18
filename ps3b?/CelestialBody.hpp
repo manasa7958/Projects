edbf89f85ae955b6afa0cef61ad8b98c56a64d18
manasa7958 <manasapraveen.work@gmail.com>
2025-03-18 18:33:12 -0400

    Update CelestialBody.hpp

--- ps3b?/CelestialBody.hpp
+++ ps3b?/CelestialBody.hpp
@@ -21,7 +21,7 @@ class CelestialBody: public sf::Drawable {
  private:
     sf::Vector2f pos_;
     sf::Vector2f vel_;
-    float weight_;
+    float m;
     std::string textureFile_;
     std::shared_ptr<sf::Texture> texture_;
     std::shared_ptr<sf::Sprite> sprite_;
