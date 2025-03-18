b6fe24e9c2999f0aec3408925e522e66d6904867
manasa7958 <manasapraveen.work@gmail.com>
2025-03-18 19:43:02 -0400

    Update main.cpp

--- ps3b?/main.cpp
+++ ps3b?/main.cpp
@@ -44,7 +44,7 @@ int main(int argc, char* argv[]) {
         window.clear(sf::Color(15, 15, 60));
         sf::RenderStates states;
         states.transform.scale(2.0f, 2.0f); // Adjust scaling factor as needed
-        spaceWindow.draw(universe, states);
+        window.draw(universe, states);
         window.display();
     }
 
