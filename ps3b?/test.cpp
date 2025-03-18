cfce56b98156fa6eb0ad62300bc1f80909e30552
manasa7958 <manasapraveen.work@gmail.com>
2025-03-18 17:51:08 -0400

    Update test.cpp

--- ps3b?/test.cpp
+++ ps3b?/test.cpp
@@ -7,49 +7,49 @@
 #include "Universe.hpp"
 #include "CelestialBody.hpp"
 
-BOOST_AUTO_TEST_SUITE(CelestialObjectTests)
+BOOST_AUTO_TEST_SUITE(SpaceObjectTests)
 
-BOOST_AUTO_TEST_CASE(TestDefaultConstructor) {
+BOOST_AUTO_TEST_CASE(TestCelestialConstructor) {
     NB::CelestialBody obj;
     BOOST_CHECK_EQUAL(obj.mass(), 0.0f);
     BOOST_CHECK_EQUAL(obj.position().x, 0.0f);
     BOOST_CHECK_EQUAL(obj.position().y, 0.0f);
 }
 
-BOOST_AUTO_TEST_CASE(TestInputOperator) {
-    std::stringstream ss("1.0 2.0 3.0 4.0 5.0 planet.gif");
+BOOST_AUTO_TEST_CASE(TestCelestialInputOperator) {
+    std::stringstream ss("2.0 3.0 4.0 5.0 6.0 mars.gif");
     NB::CelestialBody obj;
     ss >> obj;
-    BOOST_CHECK_CLOSE(obj.position().x, 1.0f, 0.001f);
-    BOOST_CHECK_CLOSE(obj.position().y, 2.0f, 0.001f);
-    BOOST_CHECK_CLOSE(obj.velocity().x, 3.0f, 0.001f);
-    BOOST_CHECK_CLOSE(obj.velocity().y, 4.0f, 0.001f);
-    BOOST_CHECK_CLOSE(obj.mass(), 5.0f, 0.001f);
+    BOOST_CHECK_CLOSE(obj.position().x, 2.0f, 0.001f);
+    BOOST_CHECK_CLOSE(obj.position().y, 3.0f, 0.001f);
+    BOOST_CHECK_CLOSE(obj.velocity().x, 4.0f, 0.001f);
+    BOOST_CHECK_CLOSE(obj.velocity().y, 5.0f, 0.001f);
+    BOOST_CHECK_CLOSE(obj.mass(), 6.0f, 0.001f);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
 
-BOOST_AUTO_TEST_SUITE(UniverseTests)
+BOOST_AUTO_TEST_SUITE(GalacticSimulationTests)
 
-BOOST_AUTO_TEST_CASE(TestDefaultUniverse) {
-    NB::Universe gal;
-    BOOST_CHECK_EQUAL(gal.size(), 0);
-    BOOST_CHECK_GE(gal.radius(), 0.0);
+BOOST_AUTO_TEST_CASE(TestUniverseConstructor) {
+    NB::Universe cosmos;
+    BOOST_CHECK_EQUAL(cosmos.size(), 0);
+    BOOST_CHECK_GE(cosmos.radius(), 0.0);
 }
 
-BOOST_AUTO_TEST_CASE(TestGalaxyEvolution) {
+BOOST_AUTO_TEST_CASE(TestUniverseEvolution) {
     std::stringstream ss;
-    ss << "2 2.50e+11\n";
+    ss << "2 3.00e+11\n";
     ss << "1.4960e+11 0.0000e+00 0.0000e+00 2.9800e+04 5.9740e+24 earth.gif\n";
     ss << "0.0000e+00 0.0000e+00 0.0000e+00 0.0000e+00 1.9890e+30 sun.gif\n";
 
-    NB::Universe gal;
-    ss >> gal;
+    NB::Universe cosmos;
+    ss >> cosmos;
 
     const double dt = 25000.0;
-    gal.step(dt);
+    cosmos.step(dt);
 
-    BOOST_CHECK_LT(gal[0].position().x, 1.4960e+11);
+    BOOST_CHECK_LT(cosmos[0].position().x, 1.4960e+11);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
