d0dc6bf2d0f00b83c771449559d1830b7b18fefc
manasa7958 <manasapraveen.work@gmail.com>
2025-03-18 18:16:05 -0400

    Update CelestialBody.cpp

--- ps3b?/CelestialBody.cpp
+++ ps3b?/CelestialBody.cpp
@@ -1,4 +1,4 @@
-#include "CelestialBody.hpp"
+/*#include "CelestialBody.hpp"
 #include <iomanip>
 
 namespace NB {
@@ -74,4 +74,76 @@ std::ostream& operator<<(std::ostream& os, const CelestialBody& body) {
        << body.weight_ << " " << body.textureFile_;
     return os;
 }
+}  // namespace NB*/
+
+// CelestialBody.cpp - Updated to use sf::Vector2f for position and velocity
+#include "CelestialBody.hpp"
+#include <iomanip>
+
+namespace NB {
+
+CelestialBody::CelestialBody() : pos_(0.0f, 0.0f), vel_(0.0f, 0.0f), weight_(0.0f) {
+    textureFile_ = "default.gif";
+    texture_ = std::make_shared<sf::Texture>();
+    sprite_ = std::make_shared<sf::Sprite>();
+}
+
+sf::Vector2f CelestialBody::position() const {
+    return pos_;
+}
+
+sf::Vector2f CelestialBody::velocity() const {
+    return vel_;
+}
+
+float CelestialBody::mass() const {
+    return weight_;
+}
+
+void CelestialBody::setPosition(const sf::Vector2f& pos) {
+    pos_ = pos;
+}
+
+void CelestialBody::setVelocity(const sf::Vector2f& vel) {
+    vel_ = vel;
+}
+
+bool CelestialBody::getSprite(sf::Sprite& outSprite) const {
+    if (sprite_ && texture_) {
+        outSprite = *sprite_;
+        return true;
+    }
+    return false;
+}
+
+void CelestialBody::draw(sf::RenderTarget& window, sf::RenderStates states) const {
+    if (sprite_ && texture_) {
+        window.draw(*sprite_, states);
+    }
+}
+
+std::istream& operator>>(std::istream& is, CelestialBody& body) {
+    float x, y, vx, vy, m;
+    std::string filename;
+    if (is >> x >> y >> vx >> vy >> m >> filename) {
+        body.pos_ = sf::Vector2f(x, y);
+        body.vel_ = sf::Vector2f(vx, vy);
+        body.weight_ = m;
+        body.textureFile_ = filename;
+        if (body.texture_->loadFromFile(filename)) {
+            body.sprite_->setTexture(*body.texture_);
+        }
+    }
+    return is;
+}
+
+std::ostream& operator<<(std::ostream& os, const CelestialBody& body) {
+    os << std::scientific << std::setprecision(4)
+       << body.pos_.x << " " << body.pos_.y << " "
+       << body.vel_.x << " " << body.vel_.y << " "
+       << body.weight_ << " " << body.textureFile_;
+    return os;
+}
 }  // namespace NB
+
+
