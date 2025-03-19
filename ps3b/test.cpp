3060bc1bbc5bb15d72d06d79e865e831bdefcf43
manasa7958 <manasapraveen.work@gmail.com>
2025-03-18 23:42:53 -0400

    Update test.cpp

--- ps3b/test.cpp
+++ ps3b/test.cpp
@@ -64,3 +64,79 @@ BOOST_AUTO_TEST_CASE(TestNoAcceleration) {
     BOOST_CHECK_EQUAL(universe[0].position().x, 0.0f);
     BOOST_CHECK_EQUAL(universe[0].position().y, 0.0f);
 }
+
+
+/*BOOST_AUTO_TEST_CASE(TestAntiGravity) {
+    std::stringstream ss;
+    ss << "2 3.00e+11\n";
+    ss << "1.0000e+11 0.0000e+00 0.0000e+00 0.0000e+00 5.0000e+24 sun.gif\n";
+    ss << "-1.0000e+11 0.0000e+00 0.0000e+00 0.0000e+00 5.0000e+24 earth.gif\n";
+
+    NB::Universe universe;
+    ss >> universe;
+    
+    universe.enableAntiGravity(true);
+
+    for (int i = 0; i < 5000; i++) {
+        universe.step(50000);
+    }
+
+    BOOST_CHECK_GT(universe[0].position().x, 1.0000e+11);
+    BOOST_CHECK_LT(universe[1].position().x, -1.0000e+11);
+}
+
+BOOST_AUTO_TEST_CASE(TestInvertedGravity) {
+    std::stringstream ss;
+    ss << "2 3.00e+11\n";
+    ss << "1.0000e+11 0.0000e+00 0.0000e+00 0.0000e+00 5.0000e+24 sun.gif\n";
+    ss << "-1.0000e+11 0.0000e+00 0.0000e+00 0.0000e+00 5.0000e+24 earth.gif\n";
+
+    NB::Universe universe;
+    ss >> universe;
+    universe.step(10000);
+    BOOST_CHECK_LT(universe[0].position().x, 1.0000e+11);
+    BOOST_CHECK_GT(universe[1].position().x, -1.0000e+11);
+}*/
+
+BOOST_AUTO_TEST_CASE(TestFixedTimeStep) {
+    std::stringstream ss;
+    ss << "1 1.00e+11\n";
+    ss << "0.0000e+00 0.0000e+00 1.0000e+04 0.0000e+00 5.0000e+24 saturn.gif\n";
+
+    NB::Universe universe;
+    ss >> universe;
+    universe.step(1);
+    BOOST_CHECK_CLOSE(universe[0].position().x, 1.0000e+04, 0.001f);
+}
+
+BOOST_AUTO_TEST_CASE(TestLeapfrogIntegration) {
+    std::stringstream ss;
+    ss << "1 1.00e+11\n";
+    ss << "0.0000e+00 0.0000e+00 1.0000e+04 0.0000e+00 5.0000e+24 mars.gif\n";
+
+    NB::Universe universe;
+    ss >> universe;
+    double dt = 1;
+    for (int i = 0; i < 10; ++i) {
+        universe.step(dt / 2);
+        universe.step(dt / 2);
+    }
+    BOOST_CHECK_CLOSE(universe[0].position().x, 1.0000e+05, 0.001f);
+}
+
+BOOST_AUTO_TEST_CASE(TestExtraCredit) {
+    std::stringstream input("2 1.0e+11\n"
+        "0.0 0.0 0.0 0.0 0.0 earth.gif\n"
+        "1.0e+11 0.0 0.0 0.0 0.0 mars.gif\n");
+
+    NB::Universe universe;
+    input >> universe;
+    universe.step(1.0e+6);
+
+    BOOST_REQUIRE_CLOSE(static_cast<double>(universe[0].position().x),
+    0.0, 0.0001);
+    BOOST_REQUIRE_CLOSE(static_cast<double>(universe[0].position().y),
+    0.0, 0.0001);
+    BOOST_REQUIRE_CLOSE(static_cast<double>(universe[1].position().x),
+    1.0e+11, 0.0001);
+}
