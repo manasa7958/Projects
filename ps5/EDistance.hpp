535391dff76af9943b8a1ce8dc939d8871fc4920
manasa7958 <manasapraveen.work@gmail.com>
2025-03-24 23:49:27 -0400

    Create EDistance.hpp

---
+++ ps5/EDistance.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+
+class EDistance {
+ public:
+    EDistance(const std::string& s1, const std::string& s2);
+
+    static int penalty(char a, char b);
+    static int min3(int a, int b, int c);
+
+    int optDistance();
+    std::string alignment();
+ private:
+
+};
