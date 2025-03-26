cf6101d606566781d34ff605ff4e897aff2ffb30
manasa7958 <manasapraveen.work@gmail.com>
2025-03-26 18:25:52 -0400

    Update main.cpp

--- ps4b/main.cpp
+++ ps4b/main.cpp
@@ -10,24 +10,27 @@ int main(int argc, char* argv[]) {
     }
 
     SB::Sokoban game(argv[1]);
-    sf::RenderWindow window(sf::VideoMode(game.width() * SB::Sokoban::TILE_SIZE,
-                            game.height() * SB::Sokoban::TILE_SIZE), "Sokoban");
-   while (window.isOpen()) {
+    sf::RenderWindow window(sf::VideoMode(game.width() * SB::Sokoban::TILE_SIZE, 
+    game.height() * SB::Sokoban::TILE_SIZE), "Sokoban");
+    while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
-    
             if (!game.isWon()) {
                 if (event.type == sf::Event::KeyPressed) {
-                    if (event.key.code == sf::Keyboard::W || event.key.code == sf::Keyboard::Up) {
+                    if (event.key.code == sf::Keyboard::W ||
+                        event.key.code == sf::Keyboard::Up) {
                         game.movePlayer(SB::Direction::Up);
-                    } else if (event.key.code == sf::Keyboard::S || event.key.code == sf::Keyboard::Down) {
+                    } else if (event.key.code == sf::Keyboard::S ||
+                        event.key.code == sf::Keyboard::Down) {
                         game.movePlayer(SB::Direction::Down);
-                    } else if (event.key.code == sf::Keyboard::A || event.key.code == sf::Keyboard::Left) {
+                    } else if (event.key.code == sf::Keyboard::A ||
+                        event.key.code == sf::Keyboard::Left) {
                         game.movePlayer(SB::Direction::Left);
-                    } else if (event.key.code == sf::Keyboard::D || event.key.code == sf::Keyboard::Right) {
+                    } else if (event.key.code == sf::Keyboard::D ||
+                        event.key.code == sf::Keyboard::Right) {
                         game.movePlayer(SB::Direction::Right);
                     }
                 }
@@ -41,6 +44,5 @@ int main(int argc, char* argv[]) {
     window.draw(game);
     window.display();
     }
-
     return 0;
 }
