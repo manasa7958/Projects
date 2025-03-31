1ebba99668d6ad8d784f6920f262e5ead72f67b3
manasa7958 <manasapraveen.work@gmail.com>
2025-03-30 20:42:57 -0400

    Update Sokoban.cpp

--- ps4b/Sokoban.cpp
+++ ps4b/Sokoban.cpp
@@ -11,7 +11,7 @@ namespace SB {
 
 Sokoban::Sokoban() : boardWidth(0), boardHeight(0) {}
 
-Sokoban::Sokoban(const std::string& filename){
+Sokoban::Sokoban(const std::string& filename) {
     std::ifstream file(filename);
     if (!file) throw std::runtime_error("Unable to open file");
     file >> *this;
