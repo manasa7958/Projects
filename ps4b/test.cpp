e09c8b66aa1587a74be7a6f6c274029cf8dcfbe8
manasa7958 <manasapraveen.work@gmail.com>
2025-03-30 22:01:16 -0400

    Update test.cpp

--- ps4b/test.cpp
+++ ps4b/test.cpp
@@ -105,6 +105,20 @@ BOOST_AUTO_TEST_CASE(IgnoreBoxesTest) {
     }
 }*/
 
+BOOST_AUTO_TEST_CASE(MultipleBoxVictoryTest) {
+    SB::Sokoban game("autowin2.lvl");
+    BOOST_CHECK(game.isWon());
+}
+
+BOOST_AUTO_TEST_CASE(MultipleTargetVictoryTest) {
+    SB::Sokoban game("autowin.lvl");
+    printBoard(game);
+    if (!game.isWon()) {
+        std::cout << "Game not won, inspecting board...\n";
+    }
+    BOOST_CHECK(game.isWon());
+}
+
 BOOST_AUTO_TEST_CASE(PlayerOffScreenTest) {
     SB::Sokoban game("edge_move.lvl");
     auto start = game.playerLoc();
@@ -119,18 +133,4 @@ BOOST_AUTO_TEST_CASE(BoxOffScreenTest) {
     auto start = game.playerLoc();
     game.movePlayer(SB::Direction::Right);
     BOOST_CHECK_EQUAL(game.playerLoc(), start);
-}
-
-BOOST_AUTO_TEST_CASE(MultipleBoxVictoryTest) {
-    SB::Sokoban game("autowin2.lvl");
-    BOOST_CHECK(game.isWon());
-}
-
-BOOST_AUTO_TEST_CASE(MultipleTargetVictoryTest) {
-    SB::Sokoban game("autowin.lvl");
-    printBoard(game);
-    if (!game.isWon()) {
-        std::cout << "Game not won, inspecting board...\n";
-    }
-    BOOST_CHECK(game.isWon());
 }
