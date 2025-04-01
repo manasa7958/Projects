29d27189ccdc836145c43aca052240927f9da228
manasa7958 <manasapraveen.work@gmail.com>
2025-04-01 08:58:52 -0400

    Update Sokoban.cpp

--- ps4b/Sokoban.cpp
+++ ps4b/Sokoban.cpp
@@ -145,8 +145,7 @@ void Sokoban::reset() {
         for (unsigned int x = 0; x < board[y].size(); ++x) {
             if (board[y][x] == '@') {
                 playerPosition = {x, y};
-            }
-            if (originalBoard[y][x] == 'a' && board[y][x] == 'A') {
+            } else if (originalBoard[y][x] == 'a' && board[y][x] == 'A') {
                 board[y][x] = 'B';  // Box is on the target
             }
         }
