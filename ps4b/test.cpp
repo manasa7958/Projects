7422d064019846fdec526ffeb36e52f5367721be
manasa7958 <manasapraveen.work@gmail.com>
2025-03-31 21:43:14 -0400

    Update test.cpp

--- ps4b/test.cpp
+++ ps4b/test.cpp
@@ -96,7 +96,7 @@ BOOST_AUTO_TEST_CASE(LotsOfTargetsTest) {
     BOOST_CHECK(game.isWon());
 }
 
-BOOST_AUTO_TEST_CASE(MissingSymbolTest) {
+/*BOOST_AUTO_TEST_CASE(MissingSymbolTest) {
     SB::Sokoban game("swapoff.lvl");
     game.movePlayer(SB::Direction::Right);
     game.movePlayer(SB::Direction::Right);
@@ -105,4 +105,4 @@ BOOST_AUTO_TEST_CASE(MissingSymbolTest) {
     game.movePlayer(SB::Direction::Left);
     BOOST_CHECK_EQUAL(initialPos.x, 5);
     BOOST_CHECK_EQUAL(initialPos.y, 2);
-}
+}*/
