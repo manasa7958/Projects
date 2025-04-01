8bed440731dece4ac812ebacce052b9c282ca495
manasa7958 <manasapraveen.work@gmail.com>
2025-04-01 18:08:48 -0400

    Update Sokoban.hpp

--- ps4b/Sokoban.hpp
+++ ps4b/Sokoban.hpp
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include "sfml_fix.hpp" // IRRELEVANT, using because of problems in terminal
+// #include "sfml_fix.hpp" - IRRELEVANT, using because of problems in terminal
 #include <SFML/Graphics.hpp>
 
 namespace SB {
