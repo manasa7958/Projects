9a0520474252b61f386e3ec9022b1b65710532ed
manasa7958 <manasapraveen.work@gmail.com>
2025-04-01 17:05:32 -0400

    Update sfml_fix.hpp

--- ps4b/sfml_fix.hpp
+++ ps4b/sfml_fix.hpp
@@ -1,4 +1,10 @@
-inline std::ostream& operator<<(std::ostream& os, const sf::Vector2u& vec) {
-    os << "(" << vec.x << ", " << vec.y << ")";
-    return os;
+#pragma once
+#include <string>
+
+// Specialization for SFML's Uint32
+namespace std {
+    template<>
+    struct char_traits<unsigned int> : public char_traits<wchar_t> {
+        typedef unsigned int char_type;
+    };
 }
