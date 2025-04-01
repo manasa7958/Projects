f23fbfecb9830cb988828a31b011ad26b25922d2
manasa7958 <manasapraveen.work@gmail.com>
2025-03-31 22:36:05 -0400

    Update Sokoban.hpp

--- ps4b/Sokoban.hpp
+++ ps4b/Sokoban.hpp
@@ -1,4 +1,3 @@
-// Copyright Manasa Praveen 2025
 #pragma once
 
 #include <iostream>
@@ -7,6 +6,7 @@
 #include <SFML/Graphics.hpp>
 
 namespace SB {
+
 enum class Direction {
     Up, Down, Left, Right
 };
@@ -18,40 +18,36 @@ class Sokoban : public sf::Drawable {
     Sokoban();
     explicit Sokoban(const std::string& filename);
 
-    unsigned int pixelHeight() const;  // Optional
-    unsigned int pixelWidth() const;  // Optional
-
-    unsigned int height() const;
     unsigned int width() const;
-
+    unsigned int height() const;
     sf::Vector2u playerLoc() const;
-
     bool isWon() const;
 
     void movePlayer(Direction dir);
     void reset();
-
-    void undo();  // Optional XC
-    void redo();  // Optional XC
+    int getMoveCount() const;
 
  protected:
     void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
 
  private:
-    bool gameWon = false;
-    sf::Font font;
-    unsigned int boardWidth;
-    unsigned int boardHeight;
-    sf::Vector2u playerPosition;
+    void loadTextures();
+
     std::vector<std::string> board;
     std::vector<std::string> originalBoard;
+    sf::Vector2u playerPosition;
+    unsigned int boardWidth;
+    unsigned int boardHeight;
     std::string originalLevelFile;
-    sf::Texture wallTexture;
-    sf::Texture boxTexture;
-    sf::Texture groundTexture;
-    sf::Texture storageTexture;
-    sf::Texture playerTexture;
+    bool gameWon = false;
+    int moveCount = 0;
+
+    static sf::Texture wallTexture, groundTexture, playerTexture, boxTexture, storageTexture;
+    static sf::Font font;
+    static bool texturesLoaded;
+
     friend std::ostream& operator<<(std::ostream& out, const Sokoban& s);
     friend std::istream& operator>>(std::istream& in, Sokoban& s);
 };
+
 }  // namespace SB
