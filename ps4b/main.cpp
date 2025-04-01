d0a5b10d66abace07699ad3892ce3e0b29e1122e
manasa7958 <manasapraveen.work@gmail.com>
2025-04-01 18:06:23 -0400

    Update main.cpp

--- ps4b/main.cpp
+++ ps4b/main.cpp
@@ -1,7 +1,7 @@
 // Copyright Manasa Praveen 2025
 #include <iostream>
 #include <string>
-// #include "sfml_fix.hpp" - IRRELEVANT, using because of problems in terminal
+#include "sfml_fix.hpp" // IRRELEVANT, using because of problems in terminal
 #include "Sokoban.hpp"
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
