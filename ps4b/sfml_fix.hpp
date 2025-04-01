60a31d2c8d0a1d73fad37a1ba2c800aaa1f6a9ca
manasa7958 <manasapraveen.work@gmail.com>
2025-04-01 17:14:34 -0400

    Update sfml_fix.hpp

--- ps4b/sfml_fix.hpp
+++ ps4b/sfml_fix.hpp
@@ -1,10 +1,10 @@
+// Copyright Manasa Praveen 2025
 #pragma once
 #include <string>
 
-// Specialization for SFML's Uint32
 namespace std {
-    template<>
-    struct char_traits<unsigned int> : public char_traits<wchar_t> {
-        typedef unsigned int char_type;
-    };
+template<>
+struct char_traits<unsigned int> : public char_traits<wchar_t> {
+    typedef unsigned int char_type;
+};
 }
