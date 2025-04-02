48217211497e75d83395b83af6026d25a6026478
manasa7958 <manasapraveen.work@gmail.com>
2025-04-02 19:18:39 -0400

    Update test.cpp

--- ps5/test.cpp
+++ ps5/test.cpp
@@ -14,15 +14,3 @@ BOOST_AUTO_TEST_CASE(WrongMinTest) {
     BOOST_CHECK_EQUAL(EDistance::min3(7, 7, 3), 3);
     BOOST_CHECK_EQUAL(EDistance::min3(1, 1, 1), 1);
 }
-
-BOOST_AUTO_TEST_CASE(test_alignment_from_file_lastygap8) {
-    auto [s1, s2] = readSequencesFromFile("ps5/lastygap8.txt");
-    EDistance ed(s1, s2);
-    int dist = ed.optDistance();
-    std::string alignment = ed.alignment();
-
-    BOOST_CHECK(dist > 0);
-    BOOST_CHECK(alignment.find("-") != std::string::npos);
-    BOOST_CHECK(alignment.find("a a") != std::string::npos ||
-        alignment.find("A A") != std::string::npos);
-}
