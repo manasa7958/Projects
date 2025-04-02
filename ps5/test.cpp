88580d1bcc48f2ecff854d32995992eb5d0f8c37
manasa7958 <manasapraveen.work@gmail.com>
2025-04-02 18:08:04 -0400

    Update test.cpp

--- ps5/test.cpp
+++ ps5/test.cpp
@@ -1,15 +1,16 @@
 // Copyright Manasa Praveen 2025
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <vector>
 #include "EDistance.hpp"
 
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE EDistanceTest
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_CASE(test_penalty_function) {
-    BOOST_CHECK_EQUAL(EDistance::penalty('A', 'A'), 0);
-    BOOST_CHECK_EQUAL(EDistance::penalty('A', 'T'), 1);
-    BOOST_CHECK_EQUAL(EDistance::penalty('G', 'C'), 1);
-    BOOST_CHECK_EQUAL(EDistance::penalty('T', 'T'), 0);
+BOOST_AUTO_TEST_CASE(WrongMinTest) {
+    BOOST_CHECK_EQUAL(EDistance::min3(5, 2, 8), 2);
+    BOOST_CHECK_EQUAL(EDistance::min3(7, 7, 3), 3);
+    BOOST_CHECK_EQUAL(EDistance::min3(1, 1, 1), 1);
 }
