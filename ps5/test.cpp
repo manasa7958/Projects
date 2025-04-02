c9329bf0ec10bb1e799c4e9505f1f7ff0fb05014
manasa7958 <manasapraveen.work@gmail.com>
2025-04-02 16:19:17 -0400

    Update test.cpp

--- ps5/test.cpp
+++ ps5/test.cpp
@@ -1,3 +1,8 @@
 #include <iostream>
 #include <string>
 #include "EDistance.hpp"
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE SokobanTests
+#include <boost/algorithm/string.hpp>
+#include <boost/test/unit_test.hpp>
