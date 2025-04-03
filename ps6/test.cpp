4f5d8a808b3c673183ed9626264affcffeaf569f
manasa7958 <manasapraveen.work@gmail.com>
2025-04-03 14:28:40 -0400

    Update test.cpp

--- ps6/test.cpp
+++ ps6/test.cpp
@@ -21,20 +21,20 @@ BOOST_AUTO_TEST_CASE(KZeroTest) {
     BOOST_REQUIRE_THROW(rw.freq("aaa", 'g'), std::exception);
     BOOST_REQUIRE_THROW(rw.kRand("a"), std::exception);
     BOOST_REQUIRE_THROW(rw.generate("a", 10), std::exception);
-}
+}*/
 
 BOOST_AUTO_TEST_CASE(GenerateLengthTest) {
     RandWriter rw(sample_text, 2);
     std::string result = rw.generate("ga", 10);
     BOOST_REQUIRE_EQUAL(result.size(), 10);
-}*/
-
+}
+/*
 BOOST_AUTO_TEST_CASE(GenerateStartTest) {
     RandWriter rw(sample_text, 2);
     std::string result = rw.generate("ga", 10);
     BOOST_REQUIRE_EQUAL(result.substr(0, 2), "ga");
 }
-/*
+
 BOOST_AUTO_TEST_CASE(KRandDistributionTest) {
     RandWriter rw(sample_text, 1);
 
