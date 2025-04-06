099d0b171901cb6ae68949e0ced11d444253a0ac
manasa7958 <manasapraveen.work@gmail.com>
2025-04-06 17:49:57 -0400

    Update EDistance.hpp

--- ps5/EDistance.hpp
+++ ps5/EDistance.hpp
@@ -1,7 +1,7 @@
 // Copyright Manasa Praveen and Ponita Ty 2025
 #pragma once
-
 #include <string>
+#include <vector>
 
 class EDistance {
  public:
@@ -12,5 +12,14 @@ class EDistance {
 
     int optDistance();
     std::string alignment();
+
  private:
+    std::string str1, str2;
+
+    std::vector<int> forwardScore(const std::string& s1, const std::string& s2);
+    std::vector<int> reverseScore(const std::string& s1, const std::string& s2);
+    void hirschbergAlign(
+        const std::string& s1, int start1, int end1,
+        const std::string& s2, int start2, int end2,
+        std::vector<std::string>& alignmentLines);
 };
