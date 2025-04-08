76bcf7116d965aba9d0c829299e08a06daf1de33
manasa7958 <manasapraveen.work@gmail.com>
2025-04-08 18:21:15 -0400

    Update TextWriter.cpp

--- ps6/TextWriter.cpp
+++ ps6/TextWriter.cpp
@@ -1,4 +1,24 @@
 // Copyright Manasa Praveen 2025
+
 #include <iostream>
 #include <string>
 #include "RandWriter.hpp"
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " <k> <L>\n";
+        return 1;
+    }
+
+    size_t k = std::stoi(argv[1]);
+    size_t L = std::stoi(argv[2]);
+
+    std::string input((std::istreambuf_iterator<char>(std::cin)),
+                      std::istreambuf_iterator<char>());
+
+    RandWriter rw(input, k);
+    std::string start = input.substr(0, k);
+    std::cout << rw.generate(start, L) << std::endl;
+
+    return 0;
+}
