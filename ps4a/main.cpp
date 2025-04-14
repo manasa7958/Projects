099f3a07e2dc550dc6359a7d9402ee2dfc0ec301
manasa7958 <manasapraveen.work@gmail.com>
2025-04-14 17:59:39 -0400

    Update main.cpp

--- ps4a/main.cpp
+++ ps4a/main.cpp
@@ -1,6 +1,8 @@
 // Copyright Manasa Praveen 2025
 #include <iostream>
+#include <string>
 #include "Sokoban.hpp"
+#include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 
 int main(int argc, char* argv[]) {
@@ -10,14 +12,52 @@ int main(int argc, char* argv[]) {
     }
 
     SB::Sokoban game(argv[1]);
+
     sf::RenderWindow window(sf::VideoMode(game.width() * SB::Sokoban::TILE_SIZE,
-                            game.height() * SB::Sokoban::TILE_SIZE), "Sokoban");
+        game.height() * SB::Sokoban::TILE_SIZE), "Sokoban");
+
+    // Extra Credit: Victory Fanfare!!
+    sf::SoundBuffer victoryBuffer;
+    if (!victoryBuffer.loadFromFile("victory.wav")) {
+        std::cerr << "Failed to load victory sound\n";
+        return 1;
+    }
+    sf::Sound victorySound;
+    victorySound.setBuffer(victoryBuffer);
+
+    bool playedVictorySound = false;
 
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 window.close();
+            }
+
+            if (!game.isWon()) {
+                if (event.type == sf::Event::KeyPressed) {
+                    if (event.key.code == sf::Keyboard::W ||
+                        event.key.code == sf::Keyboard::Up) {
+                        game.movePlayer(SB::Direction::Up);
+                    } else if (event.key.code == sf::Keyboard::S ||
+                        event.key.code == sf::Keyboard::Down) {
+                        game.movePlayer(SB::Direction::Down);
+                    } else if (event.key.code == sf::Keyboard::A ||
+                        event.key.code == sf::Keyboard::Left) {
+                        game.movePlayer(SB::Direction::Left);
+                    } else if (event.key.code == sf::Keyboard::D ||
+                        event.key.code == sf::Keyboard::Right) {
+                        game.movePlayer(SB::Direction::Right);
+                    }
+                }
+            } else if (!playedVictorySound) {
+                victorySound.play();
+                playedVictorySound = true;
+            }
+
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::R) {
+                game.reset();
+                playedVictorySound = false;
             }
         }
 
