640d0436f039e4ab75f08dc4677b7ed407a4d0a1
manasa7958 <manasapraveen.work@gmail.com>
2025-04-14 15:43:27 -0400

    Update WordWriter.hpp

--- ps6/WordWriter.hpp
+++ ps6/WordWriter.hpp
@@ -7,24 +7,20 @@
 #include <iostream>
 
 class WordWriter {
-    public:
-        WordWriter(const std::string& text, size_t k);
-    
-        size_t orderK() const;
-        int freq(const std::string& kgram) const;
-        int freq(const std::string& kgram, const std::string& word) const;
-        std::string kRand(const std::string& kgram);
-        std::string generate(const std::string& kgram, size_t L);
-    
-        friend std::ostream& operator<<(std::ostream& os, const WordWriter& ww);
-
-    private:
-        size_t k;
-        std::vector<std::string> words;
-        std::unordered_map<std::string, std::unordered_map<std::string, int>> kgram_map;
-        std::unordered_map<std::string, int> kgram_count;
-        std::vector<std::string> alphabet;
-        std::mt19937 gen;
-    
-        std::vector<std::string> tokenize(const std::string& text) const;
+ public:
+    WordWriter(const std::string& text, size_t k);
+    size_t orderK() const;
+    int freq(const std::string& kgram) const;
+    int freq(const std::string& kgram, const std::string& word) const;
+    std::string kRand(const std::string& kgram);
+    std::string generate(const std::string& kgram, size_t L);
+    friend std::ostream& operator<<(std::ostream& os, const WordWriter& ww);
+ private:
+    size_t k;
+    std::vector<std::string> words;
+    std::unordered_map<std::string, std::unordered_map<std::string, int>> kgram_map;
+    std::unordered_map<std::string, int> kgram_count;
+    std::vector<std::string> alphabet;
+    std::mt19937 gen;
+    std::vector<std::string> tokenize(const std::string& text) const;
 };
