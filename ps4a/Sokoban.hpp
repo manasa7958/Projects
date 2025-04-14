65a9649072109b0a4a5f937be36063d47bdd8653
manasa7958 <manasapraveen.work@gmail.com>
2025-04-14 17:50:24 -0400

    Update Sokoban.hpp

--- ps4a/Sokoban.hpp
+++ ps4a/Sokoban.hpp
@@ -2,12 +2,13 @@
 #pragma once
 
 #include <iostream>
-#include <string>
 #include <vector>
-
+#include <string>
+// #include "sfml_fix.hpp" - IRRELEVANT, using because of problems in terminal
 #include <SFML/Graphics.hpp>
 
 namespace SB {
+
 enum class Direction {
     Up, Down, Left, Right
 };
@@ -17,43 +18,42 @@ class Sokoban : public sf::Drawable {
     static const int TILE_SIZE = 64;
 
     Sokoban();
-    explicit Sokoban(const std::string&);  // Optional
+    explicit Sokoban(const std::string& filename);
 
-    unsigned int pixelHeight() const;  // Optional
-    unsigned int pixelWidth() const;  // Optional
-
-    unsigned int height() const;
     unsigned int width() const;
-
+    unsigned int height() const;
     sf::Vector2u playerLoc() const;
-
     bool isWon() const;
 
     void movePlayer(Direction dir);
     void reset();
-
-    void undo();  // Optional XC
-    void redo();  // Optional XC
+    int getMoveCount() const;
 
  protected:
     void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
 
  private:
+    void loadTextures();
+
+    std::vector<std::string> board;
+    std::vector<std::string> originalBoard;
+    sf::Vector2u playerPosition;
     unsigned int boardWidth;
     unsigned int boardHeight;
-    sf::Vector2u playerPosition;
-    std::vector<std::string> board;
-    sf::Texture wallTexture;
-    sf::Texture boxTexture;
-    sf::Texture groundTexture;
-    sf::Texture storageTexture;
-    sf::Texture playerTexture;
+    std::string originalLevelFile;
+    bool gameWon = false;
+    int moveCount = 0;
+
+    static sf::Texture wallTexture, groundTexture, playerTexture, boxTexture, storageTexture;
+    static sf::Font font;
+    static bool texturesLoaded;
+
+    static sf::Texture playerTextureUp, playerTextureDown;
+    static sf::Texture playerTextureLeft, playerTextureRight;
+    Direction lastDirection = Direction::Down;
+
     friend std::ostream& operator<<(std::ostream& out, const Sokoban& s);
     friend std::istream& operator>>(std::istream& in, Sokoban& s);
-
-    // PSXc - Extra Credit
-    unsigned int moveCount;
-    sf::Font font;
-    sf::Text moveText;
 };
+
 }  // namespace SB
