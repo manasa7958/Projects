022fdd9e7034040214a7036e09e713d92b7934c8
manasa7958 <manasapraveen.work@gmail.com>
2025-04-14 18:02:46 -0400

    Update main.cpp

--- ps4a/main.cpp
+++ ps4a/main.cpp
@@ -16,17 +16,6 @@ int main(int argc, char* argv[]) {
     sf::RenderWindow window(sf::VideoMode(game.width() * SB::Sokoban::TILE_SIZE,
         game.height() * SB::Sokoban::TILE_SIZE), "Sokoban");
 
-    // Extra Credit: Victory Fanfare!!
-    sf::SoundBuffer victoryBuffer;
-    if (!victoryBuffer.loadFromFile("victory.wav")) {
-        std::cerr << "Failed to load victory sound\n";
-        return 1;
-    }
-    sf::Sound victorySound;
-    victorySound.setBuffer(victoryBuffer);
-
-    bool playedVictorySound = false;
-
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
@@ -50,9 +39,6 @@ int main(int argc, char* argv[]) {
                         game.movePlayer(SB::Direction::Right);
                     }
                 }
-            } else if (!playedVictorySound) {
-                victorySound.play();
-                playedVictorySound = true;
             }
 
             if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::R) {
