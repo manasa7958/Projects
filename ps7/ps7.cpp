63b18e8bb7e9fa36d37c63ede143c068089900bb
manasa7958 <manasapraveen.work@gmail.com>
2025-04-24 23:18:00 -0400

    Update ps7.cpp

--- ps7/ps7.cpp
+++ ps7/ps7.cpp
@@ -1,96 +1,118 @@
 // Copyright 2025 Manasa Praveen
-
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <string>
-#include <vector>
 #include <boost/regex.hpp>
-#include <boost/date_time/gregorian/gregorian.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/date_time/gregorian/gregorian.hpp>
 
-using namespace std;
-using namespace boost::gregorian;
-using namespace boost::posix_time;
+using boost::gregorian::date;
+using boost::posix_time::ptime;
+using boost::posix_time::time_duration;
+using boost::posix_time::to_iso_extended_string;
+
+struct BootState {
+    bool isBootOngoing = false;
+    ptime bootStartTime;
+    int bootStartLine = -1;
+};
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
-        cerr << "Usage: " << argv[0] << " <logfile>\n";
+        std::cerr << "Usage: " << argv[0] << " <logfile>\n";
         return 1;
     }
 
-    string input_path = argv[1];
-    ifstream infile(input_path);
-    if (!infile.is_open()) {
-        cerr << "Could not open file: " << input_path << endl;
+    const std::string inputPath = argv[1];
+    std::ifstream logFile(inputPath);
+    if (!logFile.is_open()) {
+        std::cerr << "Error: Cannot open input file " << inputPath << std::endl;
         return 1;
     }
 
-    boost::regex start_regex(R"((\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2}): \(log\.c\.166\) server started)");
-    boost::regex end_regex(R"((\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})\.(\d{3}):INFO:oejs\.AbstractConnector:Started SelectChannelConnector@0\.0\.0\.0:9080)");
+    const boost::regex startPattern(R"((\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2}): \(log\.c\.166\) server started)");
+    const boost::regex endPattern(R"((\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})\.(\d{3}):INFO:oejs\.AbstractConnector:Started SelectChannelConnector@0\.0\.0\.0:9080)");
     boost::smatch match;
 
-    ostringstream report;
-    int line_number = 1;
-    int initiated_count = 0, completed_count = 0;
-    bool awaiting = false;
-    ptime start_time;
-
-    string line;
-    while (getline(infile, line)) {
-        if (boost::regex_search(line, match, start_regex)) {
-            if (awaiting) {
-                report << "**** Incomplete boot ****\n\n";
-            }
-
-            date log_date(stoi(match[1]), stoi(match[2]), stoi(match[3]));
-            start_time = ptime(log_date, time_duration(stoi(match[4]),
-                stoi(match[5]), stoi(match[6])));
-
-            report << "=== Device boot ===\n";
-            report << line_number << "(" << input_path << "): "
-                   << to_iso_extended_string(start_time.date()) << ' '
-                   << start_time.time_of_day() << " Boot Start\n";
-
-            ++initiated_count;
-            awaiting = true;
-        } else if (boost::regex_match(line, match, end_regex)) {
-            if (awaiting) {
-                date log_date(stoi(match[1]), stoi(match[2]), stoi(match[3]));
-                ptime end_time(log_date, time_duration(stoi(match[4]),
-                stoi(match[5]), stoi(match[6])));
-                time_duration dur = end_time - start_time;
-
-                report << line_number << "(" << input_path << "): "
-                       << to_iso_extended_string(end_time.date()) << ' '
-                       << end_time.time_of_day() << " Boot Completed\n";
-                report << "\tBoot Time: " << dur.total_milliseconds() << "ms\n\n";
-
-                ++completed_count;
-                awaiting = false;
-            }
+    auto buildTimestamp = [](const boost::smatch& m) -> ptime {
+        try {
+            date d(std::stoi(m[1]), std::stoi(m[2]), std::stoi(m[3]));
+            time_duration t(std::stoi(m[4]), std::stoi(m[5]), std::stoi(m[6]));
+            return ptime(d, t);
+        } catch (...) {
+            return ptime(boost::date_time::not_a_date_time);
         }
-        ++line_number;
+    };
+
+    std::ostringstream reportStream;
+    BootState boot;
+    int currentLine = 0;
+    int totalStarts = 0;
+    int totalCompletes = 0;
+    std::string line;
+
+    while (std::getline(logFile, line)) {
+        ++currentLine;
+
+        if (boost::regex_search(line, match, startPattern)) {
+            if (boot.isBootOngoing) {
+                reportStream << "**** Incomplete boot ****\n\n";
+            }
+
+            ptime timestamp = buildTimestamp(match);
+            if (timestamp.is_not_a_date_time()) {
+                std::cerr << "Warning: Bad start timestamp on line " << currentLine << std::endl;
+                continue;
+            }
+
+            reportStream << "=== Device boot ===\n";
+            reportStream << currentLine << "(" << inputPath << "): "
+                         << to_iso_extended_string(timestamp.date()) << " "
+                         << timestamp.time_of_day() << " Boot Start\n";
+
+            boot.isBootOngoing = true;
+            boot.bootStartTime = timestamp;
+            boot.bootStartLine = currentLine;
+            ++totalStarts;
+
+        } else if (boost::regex_match(line, match, endPattern) && boot.isBootOngoing) {
+            ptime endTimestamp = buildTimestamp(match);
+            if (endTimestamp.is_not_a_date_time()) {
+                std::cerr << "Warning: Bad end timestamp on line " << currentLine << std::endl;
+                continue;
+            }
+
+            time_duration duration = endTimestamp - boot.bootStartTime;
+
+            reportStream << currentLine << "(" << inputPath << "): "
+                         << to_iso_extended_string(endTimestamp.date()) << " "
+                         << endTimestamp.time_of_day() << " Boot Completed\n";
+            reportStream << "\tBoot Time: " << duration.total_milliseconds() << "ms\n\n";
+
+            boot.isBootOngoing = false;
+            ++totalCompletes;
+        }
     }
 
-    if (awaiting) {
-        report << "**** Incomplete boot ****\n\n";
+    if (boot.isBootOngoing) {
+        reportStream << "**** Incomplete boot ****\n\n";
     }
 
-    string out_path = input_path + ".rpt";
-    ofstream outfile(out_path);
-    if (!outfile.is_open()) {
-        cerr << "Could not write to file: " << out_path << '\n';
+    const std::string outputPath = inputPath + ".rpt";
+    std::ofstream outputFile(outputPath);
+    if (!outputFile) {
+        std::cerr << "Error: Could not write report to " << outputPath << std::endl;
         return 1;
     }
 
-    outfile << "Device Boot Report\n\n";
-    outfile << "InTouch log file: " << input_path << '\n';
-    outfile << "Lines Scanned: " << line_number - 1 << "\n\n";
-    outfile << "Device boot count: initiated = " << initiated_count
-            << ", completed: " << completed_count << "\n\n\n";
-    outfile << report.str();
+    outputFile << "Device Boot Report\n\n";
+    outputFile << "InTouch log file: " << inputPath << "\n";
+    outputFile << "Lines Scanned: " << currentLine << "\n\n";
+    outputFile << "Device boot count: initiated = " << totalStarts
+               << ", completed: " << totalCompletes << "\n\n\n";
+    outputFile << reportStream.str();
 
-    cout << "Report written to " << out_path << endl;
+    std::cout << "Report written to " << outputPath << std::endl;
     return 0;
 }
