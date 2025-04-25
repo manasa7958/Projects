ffe45da2fe22b811c3900c83ea78f2d1f83409f0
manasa7958 <manasapraveen.work@gmail.com>
2025-04-24 21:07:26 -0400

    Update ps7.cpp

--- ps7/ps7.cpp
+++ ps7/ps7.cpp
@@ -1,60 +1,17 @@
+// Copyright 2025 Manasa Praveen
+
 #include <iostream>
 #include <fstream>
-#include <regex>
+#include <sstream>
 #include <string>
 #include <vector>
-#include <map>
-#include <sstream>
-#include <iomanip>
-#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/regex.hpp>
 #include <boost/date_time/gregorian/gregorian.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
 
 using namespace std;
-using namespace boost::posix_time;
 using namespace boost::gregorian;
-
-struct BootEvent {
-    int lineNumber;
-    string timestamp;
-    ptime parsedTime;
-    bool isStart;
-};
-
-struct BootSequence {
-    int startLine;
-    string startTimestamp;
-    ptime startTime;
-    int endLine;
-    string endTimestamp;
-    ptime endTime;
-    bool success;
-    time_duration duration;
-};
-
-ptime parseTimestampFromStr(const string& ts) {
-    static const regex iso_pattern(R"((\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}))");
-    static const regex alt_pattern(R"((\w{3}\s+\d{1,2}\s\d{2}:\d{2}:\d{2}))");
-
-    smatch match;
-    if (regex_match(ts, match, iso_pattern)) {
-        return time_from_string(ts);
-    }
-
-    if (regex_match(ts, match, alt_pattern)) {
-        string fullTimestamp = "2013 " + ts;
-        istringstream ss(fullTimestamp);
-        tm t = {};
-        ss >> get_time(&t, "%Y %b %d %H:%M:%S");
-
-        if (!ss.fail()) {
-            date d(t.tm_year + 1900, t.tm_mon + 1, t.tm_mday);
-            time_duration td(t.tm_hour, t.tm_min, t.tm_sec);
-            return ptime(d, td);
-        }
-    }
-
-    return ptime(not_a_date_time);
-}
+using namespace boost::posix_time;
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
@@ -62,117 +19,76 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    ifstream infile(argv[1]);
+    string input_path = argv[1];
+    ifstream infile(input_path);
     if (!infile.is_open()) {
-        cerr << "Could not open file: " << argv[1] << endl;
+        cerr << "Could not open file: " << input_path << endl;
         return 1;
     }
 
-    const regex start_pattern(R"((\w{3}\s+\d{1,2}\s\d{2}:\d{2}:\d{2}).*\(log\.c\.166\) server started)");
-    const regex end_pattern(R"((\w{3}\s+\d{1,2}\s\d{2}:\d{2}:\d{2}).*oejs\.AbstractConnector:Started SelectChannelConnector)");
+    boost::regex start_regex(R"((\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2}): \(log\.c\.166\) server started)");
+    boost::regex end_regex(R"((\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})\.(\d{3}):INFO:oejs\.AbstractConnector:Started SelectChannelConnector@0\.0\.0\.0:9080)");
+    boost::smatch match;
 
-    vector<BootEvent> events;
-    string line;
-    int lineNum = 0;
-
-    while (getline(infile, line)) {
-        ++lineNum;
-        smatch match;
-
-        if (regex_search(line, match, start_pattern)) {
-            string ts = match.str(1);
-            ptime timestamp = parseTimestampFromStr(ts);
-            if (!timestamp.is_not_a_date_time()) {
-                events.push_back({lineNum, ts, timestamp, true});
-            }
-        } else if (regex_search(line, match, end_pattern)) {
-            string ts = match.str(1);
-            ptime timestamp = parseTimestampFromStr(ts);
-            if (!timestamp.is_not_a_date_time()) {
-                events.push_back({lineNum, ts, timestamp, false});
-            }
-        }
-    }
-
-    vector<BootSequence> sequences;
-    BootEvent pendingStart;
-    bool hasPending = false;
-
-    for (const auto& event : events) {
-        if (event.isStart) {
-            if (hasPending) {
-                BootSequence seq;
-                seq.startLine = pendingStart.lineNumber;
-                seq.startTimestamp = pendingStart.timestamp;
-                seq.startTime = pendingStart.parsedTime;
-                seq.success = false;
-                sequences.push_back(seq);
-            }
-            pendingStart = event;
-            hasPending = true;
-        } else if (hasPending) {
-            BootSequence seq;
-            seq.startLine = pendingStart.lineNumber;
-            seq.startTimestamp = pendingStart.timestamp;
-            seq.startTime = pendingStart.parsedTime;
-
-            seq.endLine = event.lineNumber;
-            seq.endTimestamp = event.timestamp;
-            seq.endTime = event.parsedTime;
-            seq.success = true;
-            seq.duration = seq.endTime - seq.startTime;
-
-            sequences.push_back(seq);
-            hasPending = false;
-        }
-    }
-
-    if (hasPending) {
-        BootSequence seq;
-        seq.startLine = pendingStart.lineNumber;
-        seq.startTimestamp = pendingStart.timestamp;
-        seq.startTime = pendingStart.parsedTime;
-        seq.success = false;
-        sequences.push_back(seq);
-    }
-
-    string outFileName = string(argv[1]) + ".rpt";
-    ofstream outfile(outFileName);
-    if (!outfile.is_open()) {
-        cerr << "Could not create output file: " << outFileName << endl;
-        return 1;
-    }
-
+    ostringstream report;
+    int line_number = 1;
     int initiated_count = 0, completed_count = 0;
+    bool awaiting = false;
+    ptime start_time;
+
+    string line;
+    while (getline(infile, line)) {
+        if (boost::regex_search(line, match, start_regex)) {
+            if (awaiting) {
+                report << "**** Incomplete boot ****\n\n";
+            }
+
+            date log_date(stoi(match[1]), stoi(match[2]), stoi(match[3]));
+            start_time = ptime(log_date, time_duration(stoi(match[4]), stoi(match[5]), stoi(match[6])));
+
+            report << "=== Device boot ===\n";
+            report << line_number << "(" << input_path << "): "
+                   << to_iso_extended_string(start_time.date()) << ' '
+                   << start_time.time_of_day() << " Boot Start\n";
+
+            ++initiated_count;
+            awaiting = true;
+        } else if (boost::regex_match(line, match, end_regex)) {
+            if (awaiting) {
+                date log_date(stoi(match[1]), stoi(match[2]), stoi(match[3]));
+                ptime end_time(log_date, time_duration(stoi(match[4]), stoi(match[5]), stoi(match[6])));
+                time_duration dur = end_time - start_time;
+
+                report << line_number << "(" << input_path << "): "
+                       << to_iso_extended_string(end_time.date()) << ' '
+                       << end_time.time_of_day() << " Boot Completed\n";
+                report << "\tBoot Time: " << dur.total_milliseconds() << "ms\n\n";
+
+                ++completed_count;
+                awaiting = false;
+            }
+        }
+        ++line_number;
+    }
+
+    if (awaiting) {
+        report << "**** Incomplete boot ****\n\n";
+    }
+
+    string out_path = input_path + ".rpt";
+    ofstream outfile(out_path);
+    if (!outfile.is_open()) {
+        cerr << "Could not write to file: " << out_path << '\n';
+        return 1;
+    }
 
     outfile << "Device Boot Report\n\n";
-    outfile << "InTouch log file: " << argv[1] << '\n';
-    outfile << "Lines Scanned: " << lineNum << "\n\n";
+    outfile << "InTouch log file: " << input_path << '\n';
+    outfile << "Lines Scanned: " << line_number - 1 << "\n\n";
+    outfile << "Device boot count: initiated = " << initiated_count
+            << ", completed: " << completed_count << "\n\n\n";
+    outfile << report.str();
 
-    for (const auto& seq : sequences) {
-        outfile << "=== Device boot ===\n";
-
-        outfile << seq.startLine << "(" << argv[1] << "): "
-                << to_iso_extended_string(seq.startTime.date()) << ' '
-                << seq.startTime.time_of_day() << " Boot Start\n";
-
-        ++initiated_count;
-
-        if (seq.success) {
-            outfile << seq.endLine << "(" << argv[1] << "): "
-                    << to_iso_extended_string(seq.endTime.date()) << ' '
-                    << seq.endTime.time_of_day() << " Boot Completed\n";
-
-            outfile << "\tBoot Time: " << seq.duration.total_milliseconds() << "ms\n\n";
-            ++completed_count;
-        } else {
-            outfile << "**** Incomplete boot ****\n\n";
-        }
-    }
-
-    outfile << "\nDevice boot count: initiated = " << initiated_count
-            << ", completed: " << completed_count << "\n\n";
-
-    cout << "Report written to " << outFileName << endl;
+    cout << "Report written to " << out_path << endl;
     return 0;
 }
