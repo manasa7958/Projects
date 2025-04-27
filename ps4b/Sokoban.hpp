3dc4da2fc59b2522930be239ca773dca847ad1e1
manasa7958 <manasapraveen.work@gmail.com>
2025-04-27 00:45:27 -0400

    Update Sokoban.hpp

--- ps4b/Sokoban.hpp
+++ ps4b/Sokoban.hpp
@@ -11,64 +11,64 @@ namespace SB {
 enum class Direction { Up, Down, Left, Right };
 
 class Sokoban : public sf::Drawable {
-public:
-    static const unsigned int TILE_SIZE = 64;
+    public:
+        static const unsigned int TILE_SIZE = 64;
 
-    Sokoban();
-    Sokoban(const std::string& filename);
+        Sokoban();
+        Sokoban(const std::string& filename);
 
-    void movePlayer(Direction dir);
-    void reset();
-    bool isWon() const;
-    int getMoveCount() const;
-    sf::Vector2u playerLoc() const;
-    unsigned int width() const;
-    unsigned int height() const;
-    void undoMove();  // Undo feature!
+        void movePlayer(Direction dir);
+        void reset();
+        bool isWon() const;
+        int getMoveCount() const;
+        sf::Vector2u playerLoc() const;
+        unsigned int width() const;
+        unsigned int height() const;
+        void undoMove();  // Undo feature!
 
-    friend std::ostream& operator<<(std::ostream& out, const Sokoban& s);
-    friend std::istream& operator>>(std::istream& in, Sokoban& s);
+        friend std::ostream& operator<<(std::ostream& out, const Sokoban& s);
+        friend std::istream& operator>>(std::istream& in, Sokoban& s);
 
-private:
-    struct GameState {
+    private:
+        struct GameState {
+            std::vector<std::string> board;
+            sf::Vector2u playerPosition;
+            int moveCount;
+        };
+
+        std::vector<GameState> history;
+
         std::vector<std::string> board;
+        std::vector<std::string> originalBoard;
+        std::string originalLevelFile;
+        unsigned int boardWidth;
+        unsigned int boardHeight;
         sf::Vector2u playerPosition;
-        int moveCount;
-    };
+        int moveCount = 0;
+        Direction lastDirection;
+        bool gameWon = false;
 
-    std::vector<GameState> history;
+        sf::Text moveCounterText;
 
-    std::vector<std::string> board;
-    std::vector<std::string> originalBoard;
-    std::string originalLevelFile;
-    unsigned int boardWidth;
-    unsigned int boardHeight;
-    sf::Vector2u playerPosition;
-    int moveCount = 0;
-    Direction lastDirection;
-    bool gameWon = false;
+        static sf::Texture wallTexture;
+        static sf::Texture groundTexture;
+        static sf::Texture boxTexture;
+        static sf::Texture storageTexture;
+        static sf::Texture playerTexture;
+        static sf::Texture playerTextureUp;
+        static sf::Texture playerTextureDown;
+        static sf::Texture playerTextureLeft;
+        static sf::Texture playerTextureRight;
+        static sf::Font font;
+        static bool texturesLoaded;
 
-    sf::Text moveCounterText;
+        void loadTextures();
+        void saveState();
 
-    static sf::Texture wallTexture;
-    static sf::Texture groundTexture;
-    static sf::Texture boxTexture;
-    static sf::Texture storageTexture;
-    static sf::Texture playerTexture;
-    static sf::Texture playerTextureUp;
-    static sf::Texture playerTextureDown;
-    static sf::Texture playerTextureLeft;
-    static sf::Texture playerTextureRight;
-    static sf::Font font;
-    static bool texturesLoaded;
+        void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
 
-    void loadTextures();
-    void saveState();
-
-    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
-
-    sf::SoundBuffer victoryBuffer;
-    sf::Sound victorySound;
+        sf::SoundBuffer victoryBuffer;
+        sf::Sound victorySound;
 };
 
-} // namespace SB
+}  // namespace SB
